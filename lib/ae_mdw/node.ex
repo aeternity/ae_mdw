03972257e557b8633777bf5a19dccfa8675dcd3f
skkw <skkw@users.noreply.github.com>
2020-03-25 15:56:29 +0100

    refactoring in streaming functionality

--- lib/ae_mdw/node.ex
+++ lib/ae_mdw/node.ex
@@ -23,4 +23,63 @@ defmodule AeMdw.Node do
       end
     )
   end
+
+
+  defmodule Db do
+
+    alias AeMdw.Db.Model
+    require Model
+
+    # we require that block index is in place
+    import AeMdw.Db.Util, only: [read_block!: 1]
+
+
+    def get_blocks(height) when is_integer(height) do
+      kb_hash = Model.block(read_block!({height, -1}), :hash)
+      {:aec_db.get_block(kb_hash), get_micro_blocks(height)}
+    end
+
+    def get_micro_blocks(height) when is_integer(height),
+      do: do_get_micro_blocks(Model.block(read_block!({height + 1, -1}), :hash))
+
+    defp do_get_micro_blocks(<<next_gen_kb_hash::binary>>) do
+      :aec_db.get_header(next_gen_kb_hash)
+      |> :aec_headers.prev_hash
+      |> Stream.unfold(&micro_block_walker/1)
+      |> Enum.reverse
+    end
+
+    def micro_block_walker(hash) do
+      with block  <- :aec_db.get_block(hash),
+           :micro <- :aec_blocks.type(block) do
+        {block, :aec_blocks.prev_hash(block)}
+      else
+        :key -> nil
+      end
+    end
+
+  end
+
+
+  defmodule Stream do
+
+    def header_idx_tx(height) do
+      Elixir.Stream.resource(
+        fn -> {[], Db.get_micro_blocks(height), nil, -1} end,
+        &stream_next/1,
+        &AeMdw.Util.id/1
+      )
+    end
+
+    def stream_next({[tx | txs], blocks, header, mbi}),
+      do: {[{header, mbi, tx}], {txs, blocks, header, mbi}}
+    def stream_next({[], [], _, _}),
+      do: {:halt, :done}
+    def stream_next({[], [block | blocks], _, mbi}) do
+      header = :aec_blocks.to_micro_header(block)
+      stream_next({:aec_blocks.txs(block), blocks, header, mbi + 1})
+    end
+
+  end
+
 end
