8dcd263b8865c264232d1de90baaab8793b6e1cd
skkw <skkw@users.noreply.github.com>
2020-03-29 17:01:19 +0200

    using `aetx_sign.serialize_for_client/1` to get wanted output

--- lib/ae_mdw/db/model.ex
+++ lib/ae_mdw/db/model.ex
@@ -112,157 +112,38 @@ defmodule AeMdw.Db.Model do
     )
   end
 
+
   def to_raw_map({:tx, index, hash, {kb_index, mb_index}}) do
     {_, _, db_stx} = one!(:mnesia.dirty_read(:aec_signed_tx, hash))
     aec_signed_tx = :aetx_sign.from_db_format(db_stx)
     {type, rec} = :aetx.specialize_type(:aetx_sign.tx(aec_signed_tx))
-
+    tx_map = tx_record_to_map(type, rec) |> put_in([:type], type)
     %{
       block_hash: block(read_block!({kb_index, mb_index}), :hash),
-      block_height: kb_index,
-      mb_index: mb_index,
-      hash: hash,
-      type: type,
-      index: index,
       signatures: :aetx_sign.signatures(aec_signed_tx),
-      tx: tx_record_to_map(type, rec)
+      hash: hash,
+      block_height: kb_index,
+      micro_index: mb_index,
+      tx_index: index,
+      tx: tx_map
     }
   end
 
-  def to_map({:tx, index, hash, {kb_index, mb_index}}) do
-    raw = to_raw_map({:tx, index, hash, {kb_index, mb_index}})
-
-    raw
-    |> update_in([:block_hash], &Enc.encode(:micro_block_hash, &1))
-    |> update_in([:hash], &Enc.encode(:tx_hash, &1))
-    |> update_in([:signatures], fn ts -> Enum.map(ts, &Enc.encode(:signature, &1)) end)
-    |> update_in([:tx], &encode_ids(&1, raw.type))
-    |> put_in([:tx, :type], AeMdwWeb.Util.to_user_tx_type(raw.type))
-    |> custom_encode
+  def to_map({:tx, index, hash, {_kb_index, mb_index}}) do
+    {block_hash, signed_tx} = :aec_db.find_tx_with_location(hash)
+    {type, _} = :aetx.specialize_type(:aetx_sign.tx(signed_tx))
+    header = :aec_db.get_header(block_hash)
+    enc_tx = :aetx_sign.serialize_for_client(header, signed_tx)
+    custom_encode(type, enc_tx)
+    |> put_in(["tx_index"], index)
+    |> put_in(["micro_index"], mb_index)
   end
 
-  def encode_ids(tx_map, type) do
-    AeMdw.Node.tx_ids(type)
-    |> Enum.reduce(tx_map, fn {k, _}, tx -> update_in(tx[k], &encode_id/1) end)
-  end
 
-  ################################################################################
-
-  def custom_encode(%{type: :name_update_tx} = tx),
-    do: update_in(tx.tx.pointers, &encode_name_pointer/1)
-
-  def custom_encode(%{type: :contract_call_tx} = tx) do
-    tx
-    |> update_in([:tx, :call_data], &Enc.encode(:contract_bytearray, &1))
-    |> update_in([:tx, :call_origin], &Enc.encode(:account_pubkey, &1))
-  end
-
-  def custom_encode(%{type: :contract_create_tx} = tx) do
-    tx
-    |> update_in([:tx, :call_data], &Enc.encode(:contract_bytearray, &1))
-    |> update_in([:tx, :code], &Enc.encode(:contract_bytearray, &1))
-  end
-
-  def custom_encode(%{type: :channel_create_tx} = tx),
-    do: update_in(tx.tx.state_hash, &Enc.encode(:block_state_hash, &1))
-
-  def custom_encode(%{type: :channel_deposit_tx} = tx),
-    do: update_in(tx.tx.state_hash, &Enc.encode(:block_state_hash, &1))
-
-  def custom_encode(%{type: :channel_force_progress_tx} = tx) do
-    tx
-    |> update_in([:tx, :state_hash], &Enc.encode(:block_state_hash, &1))
-    |> update_in([:tx, :payload], &Enc.encode(:transaction, &1))
-    |> update_in([:tx, :update], &:aesc_offchain_update.for_client(&1))
-    |> update_in(
-      [:tx, :offchain_trees],
-      &Enc.encode(:state_trees, :aec_trees.serialize_to_binary(&1))
-    )
-  end
-
-  def custom_encode(%{type: :channel_close_solo_tx} = tx) do
-    tx
-    |> update_in([:tx, :payload], &Enc.encode(:transaction, &1))
-    |> update_in([:tx, :poi], &Enc.encode(:poi, :aec_trees.serialize_poi(&1)))
-  end
-
-  def custom_encode(%{type: :channel_slash_tx} = tx) do
-    tx
-    |> update_in([:tx, :payload], &Enc.encode(:transaction, &1))
-    |> update_in([:tx, :poi], &Enc.encode(:poi, :aec_trees.serialize_poi(&1)))
-  end
-
-  def custom_encode(%{type: :channel_snapshot_solo_tx} = tx),
-    do: update_in(tx.tx.payload, &Enc.encode(:transaction, &1))
-
-  def custom_encode(%{type: :oracle_register_tx, block_height: block_height} = tx),
-    do: update_in(tx.tx.oracle_ttl, &encode_ttl(&1, block_height))
-
-  def custom_encode(%{type: :oracle_extend_tx} = tx),
-    do:
-      put_in(tx.tx.oracle_ttl, %{
-        type: elem(tx.tx.oracle_ttl, 0),
-        value: elem(tx.tx.oracle_ttl, 1)
-      })
-
-  def custom_encode(%{type: :oracle_query_tx, block_height: block_height} = tx) do
-    tx
-    |> update_in([:tx, :query_ttl], &encode_ttl(&1, block_height))
-    |> update_in([:tx, :response_ttl], &encode_ttl(&1, block_height))
-  end
-
-  def custom_encode(%{type: :oracle_response_tx, block_height: block_height} = tx) do
-    tx
-    |> update_in([:tx, :response_ttl], &encode_ttl(&1, block_height))
-    |> update_in([:tx, :query_id], &Enc.encode(:oracle_query_id, &1))
-    |> update_in([:tx, :response], &maybe_base64/1)
-  end
-
-  def custom_encode(%{type: :ga_attach_tx} = tx) do
-    tx
-    |> update_in([:tx, :auth_fun], &encode16_lowercased/1)
-    |> update_in([:tx, :call_data], &Enc.encode(:contract_bytearray, &1))
-    |> update_in([:tx, :code], &Enc.encode(:contract_bytearray, &1))
-  end
-
-  def custom_encode(%{type: :ga_meta_tx} = tx) do
-    tx
-    |> update_in([:tx, :auth_data], &Enc.encode(:contract_bytearray, &1))
-    |> update_in([:tx, :tx], fn aec_signed_tx ->
-      {type, rec} = :aetx.specialize_type(:aetx_sign.tx(aec_signed_tx))
-
-      nested_tx =
-        tx_record_to_map(type, rec)
-        |> encode_ids(type)
-        |> put_in([:type], AeMdwWeb.Util.to_user_tx_type(type))
-
-      %{tx: nested_tx}
-    end)
-  end
-
-  def custom_encode(tx),
+  def custom_encode(:oracle_response_tx, tx),
+    do: update_in(tx, ["tx", "response"], &maybe_base64/1)
+  def custom_encode(_, tx),
     do: tx
-
-  def encode_id(xs) when is_list(xs),
-    do: xs |> Enum.map(&Enc.encode(:id_hash, &1))
-
-  def encode_id({:id, _, _} = x),
-    do: Enc.encode(:id_hash, x)
-
-  def encode_name_pointer(ptrs) when is_list(ptrs),
-    do: ptrs |> Enum.map(&encode_name_pointer/1)
-
-  def encode_name_pointer(ptr),
-    do: %{key: :aens_pointer.key(ptr), id: encode_id(:aens_pointer.id(ptr))}
-
-  def encode_ttl({:delta, diff}, height),
-    do: height + diff
-
-  def encode_ttl({:block, height}, _),
-    do: height
-
-  def encode16_lowercased(bin),
-    do: "0x" <> (bin |> Base.encode16() |> String.downcase())
 
   def maybe_base64(bin) do
     try do
