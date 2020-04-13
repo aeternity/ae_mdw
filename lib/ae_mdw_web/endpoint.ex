fd5007ff5dbf412c2699382e4e8d0241cb220a59
danielaivanova <danielaivanova_@abv.bg>
2020-04-13 13:54:52 +0300

    #5 Minor adjustment

--- lib/ae_mdw_web/endpoint.ex
+++ lib/ae_mdw_web/endpoint.ex
@@ -4,11 +4,11 @@ defmodule AeMdwWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  # @session_options [
-  #   store: :cookie,
-  #   key: "_ae_mdw_key",
-  #   signing_salt: "0cOe/zsp"
-  # ]
+  @session_options [
+    store: :cookie,
+    key: "_ae_mdw_key",
+    signing_salt: "0cOe/zsp"
+  ]
 
   # socket "/", AeMdwWeb.UserSocket,
   #   websocket: true,
@@ -18,11 +18,11 @@ defmodule AeMdwWeb.Endpoint do
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  # plug Plug.Static,
-  #   at: "/",
-  #   from: :ae_mdw,
-  #   gzip: false,
-  #   only: ~w(css fonts images js favicon.ico robots.txt)
+  plug Plug.Static,
+    at: "/",
+    from: :ae_mdw,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -42,6 +42,6 @@ defmodule AeMdwWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-  # plug Plug.Session, @session_options
+  plug Plug.Session, @session_options
   plug AeMdwWeb.Router
 end
