fbc93c266594bd1f7c559c1f78c3a7c052871310
skkw <skkw@users.noreply.github.com>
2020-04-13 11:33:46 +0200

    add ID validation

--- lib/ae_mdw/validate.ex
+++ lib/ae_mdw/validate.ex
@@ -1,12 +1,13 @@
 defmodule AeMdw.Validate do
   alias AeMdw.Node, as: AE
   alias AeMdw.Error.Input, as: ErrInput
+  alias :aeser_api_encoder, as: Enc
 
   # returns pubkey
-  def id(<<prefix::2, "_", _::binary>> = id) do
+  def id(<<prefix::2-binary, "_", _::binary>> = id) do
     case prefix in AE.id_prefixes() do
       true ->
-        {_id_type, pk} = :aeser_api_encoder.decode(id)
+        {_id_type, pk} = Enc.decode(id)
         {:ok, pk}
       false ->
         {:error, {ErrInput.Id, id}}
@@ -20,6 +21,27 @@ defmodule AeMdw.Validate do
     do: {:error, {ErrInput.Id, id}}
 
   def id!(id), do: unwrap!(&id/1, id)
+
+
+  def id(<<prefix::2-binary, "_", _::binary>> = ident, [_|_] = allowed_types) do
+    case prefix in AE.id_prefixes() do
+      true ->
+        case Enc.safe_decode({:id_hash, allowed_types}, ident) do
+          {:ok, id} ->
+            id(id)
+          {:error, _reason} ->
+            {:error, {ErrInput.Id, ident}}
+        end
+      false ->
+        {:error, {ErrInput.Id, ident}}
+    end
+  end
+  def id({:id, hash_type, <<_::256>> = pk} = id, [_|_] = allowed_types),
+    do: AE.id_type(hash_type) in allowed_types && {:ok, pk} || {:error, {ErrInput.Id, id}}
+  def id(id, _),
+    do: {:error, {ErrInput.Id, id}}
+
+  def id!(id, allowed_types), do: unwrap!(&id(&1, allowed_types), id)
 
   # returns transaction type (atom)
   def tx_type(type) when is_atom(type),
