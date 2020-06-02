92765d1faf0b8aa5356df5e3498bcb4e6b012711
skkw <skkw@users.noreply.github.com>
2020-06-02 09:39:47 +0200

    refactor error module

--- lib/ae_mdw/error.ex
+++ lib/ae_mdw/error.ex
@@ -1,36 +1,33 @@
 defmodule AeMdw.Error do
+
+  alias AeMdw.Error.Input, as: Err
+
+  import AeMdwWeb.Util, only: [concat: 2]
+
+  def to_string(Err.Id, x), do: concat("invalid id", x)
+  def to_string(Err.NonnegInt, x), do: concat("invalid non-negative integer", x)
+  def to_string(Err.TxField, x), do: concat("invalid transaction field", x)
+  def to_string(Err.TxType, x), do: concat("invalid transaction type", x)
+  def to_string(Err.TxGroup, x), do: concat("invalid transaction group", x)
+  def to_string(Err.Scope, x), do: concat("invalid scope", x)
+  def to_string(Err.Query, x), do: concat("invalid query", x)
+
+
   defmodule Input do
     defexception [:message]
 
-    def struct(prefix, val),
-      do: %AeMdw.Error.Input{message: AeMdwWeb.Util.concat(prefix, val)}
+    defmodule Id, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
 
-    defmodule Id do
-      def exception(value: value), do: Input.struct("invalid id", value)
-    end
+    defmodule NonnegInt, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
 
-    defmodule NonnegInt do
-      def exception(value: value), do: Input.struct("invalid non-negative integer", value)
-    end
+    defmodule TxField, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
 
-    defmodule TxField do
-      def exception(value: value), do: Input.struct("invalid transaction field", value)
-    end
+    defmodule TxType, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
 
-    defmodule TxType do
-      def exception(value: value), do: Input.struct("invalid transaction type", value)
-    end
+    defmodule TxGroup, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
 
-    defmodule TxGroup do
-      def exception(value: value), do: Input.struct("invalid transaction group", value)
-    end
+    defmodule Scope, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
 
-    defmodule Scope do
-      def exception(value: value), do: Input.struct("invalid scope", value)
-    end
-
-    defmodule Query do
-      def exception(value: value), do: Input.struct("invalid query", value)
-    end
+    defmodule Query, do: def exception(value: value), do: AeMdw.Error.to_string(__MODULE__, value)
   end
 end
