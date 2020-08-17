3085530b2e42335081ac5af56bcdd3e0ec15c706
danielaivanova <danielaivanova_@abv.bg>
2020-08-17 19:05:58 +0300

    Fixed worker and added doc

--- lib/ae_mdw_web/benchmark/worker.ex
+++ lib/ae_mdw_web/benchmark/worker.ex
@@ -5,7 +5,7 @@ defmodule AeMdwWeb.Benchmark.Worker do
 
   def state(pid), do: GenServer.call(pid, :state)
 
-  def prepare(pid, scenario), do: GenServer.call(pid, {:start, scenario})
+  def prepare(pid, scenario), do: GenServer.cast(pid, {:start, scenario})
 
   def init(state) do
     {:ok, state}
@@ -15,7 +15,7 @@ defmodule AeMdwWeb.Benchmark.Worker do
     {:reply, state, state}
   end
 
-  def handle_call({:start, scenario}, _from, _state) do
+  def handle_cast({:start, scenario}, _state) do
     new_state =
       Enum.reduce(scenario, %{}, fn url, acc ->
         fun = fn -> Client.build_request(url) end
@@ -23,6 +23,6 @@ defmodule AeMdwWeb.Benchmark.Worker do
         Map.put(acc, url, %{time: time, status: status})
       end)
 
-    {:reply, new_state, new_state}
+    {:noreply, new_state}
   end
 end
