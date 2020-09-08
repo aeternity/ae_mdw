7b40947a9d04d6ea5963cda7e8bf542aeeff1ff8
danielaivanova <danielaivanova_@abv.bg>
2020-09-08 14:21:55 +0300

    #77 Fixed mdw status test

--- test/ae_mdw_web/controllers/util_controller_test.exs
+++ test/ae_mdw_web/controllers/util_controller_test.exs
@@ -5,14 +5,18 @@ defmodule AeMdwWeb.UtilControllerTest do
     test "get middleware status", %{conn: conn} do
       {:ok, top_kb} = :aec_chain.top_key_block()
       {_, _, node_vsn} = Application.started_applications() |> List.keyfind(:aecore, 0)
+      node_height = :aec_blocks.height(top_kb)
+      mdw_height = AeMdw.Db.Util.last_gen()
 
       conn = get(conn, "/status")
 
       assert json_response(conn, 200) == %{
                "mdw_version" => AeMdw.MixProject.project()[:version],
                "node_version" => to_string(node_vsn),
-               "mdw_height" => AeMdw.Db.Util.last_gen(),
-               "node_height" => :aec_blocks.height(top_kb)
+               "mdw_height" => mdw_height,
+               "node_height" => node_height,
+               "mdw_tx_index" => AeMdw.Db.Util.last_txi(),
+               "mdw_synced" => node_height == mdw_height
              }
     end
   end
