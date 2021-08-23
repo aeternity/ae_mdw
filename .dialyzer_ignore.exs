2a455a70cc48e68f017143a35c5c1679eeffb004
Sebastian Borrazas <seba.borrazas@gmail.com>
2021-08-23 10:18:04 -0300

    docs: Update .dialyzer_ignore.exs
    Co-authored-by: Artur64 <30468851+Artur64@users.noreply.github.com>

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,7 +2,7 @@
   # The "does not exist" warning happens because of all of the functions
   # created during runtime that belong to the AE core code.
   ~r/does not exist/,
-  # Following warnings are due to bad implementatino of riverside.
+  # Following warnings are due to bad implementation of riverside.
   {"lib/phoenix/router.ex", :pattern_match, 402},
   {"lib/riverside.ex", :pattern_match, 876},
   {"lib/ae_mdw_web/websocket/socket_handler.ex", :overlapping_contract, 2}
