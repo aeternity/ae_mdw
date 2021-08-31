a39cac6345fb618a681dc77fd26ef32a26521092
Rogerio Pontual <44991200+jyeshe@users.noreply.github.com>
2021-08-31 09:28:15 +0100

    feat: backup and restore db table (#227)

---
+++ test/mix/tasks/tasks_test.exs
@@ -0,0 +1,28 @@
+defmodule AeMdw.MixTasksTest do
+  use ExUnit.Case
+
+  alias AeMdw.Db.Model
+  alias AeMdw.Db.Util
+  alias Mix.Tasks.BackupDbTable
+
+  test "backup and restore db table" do
+    table = Model.ContractLog
+    backup_table = BackupDbTable.backup_table(table)
+    {:ok, record_name} = BackupDbTable.create_backup_table(table, backup_table)
+
+    IO.puts("testing backup for #{table} with #{record_name} records")
+
+    records_before = Util.select(table, [{:"$1", [], [:"$1"]}])
+
+    backup_count = BackupDbTable.copy_records(table, backup_table)
+    assert backup_count == length(records_before)
+
+    IO.puts("restoring ...")
+
+    restore_count = BackupDbTable.copy_records(table, backup_table)
+    assert restore_count == length(records_before)
+
+    records_after = Util.select(table, [{:"$1", [], [:"$1"]}])
+    assert records_after == records_before
+  end
+end
