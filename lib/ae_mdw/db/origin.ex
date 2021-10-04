edd1c6fb902af5e1f49af5c23f5340a02d0fb5ff
Sebastian Borrazas <seba.borrazas@gmail.com>
2021-10-03 21:44:24 -0300

    fix: return nil when contract tries fetching non-synced tx

--- lib/ae_mdw/db/origin.ex
+++ lib/ae_mdw/db/origin.ex
@@ -9,18 +9,28 @@ defmodule AeMdw.Db.Origin do
 
   ##########
 
+  @spec block_index({:contract, binary()}) :: map()
   def block_index({:contract, id}),
     do: map_some(tx_index({:contract, id}), &Model.tx(read_tx!(&1), :block_index))
 
+  @spec tx_index({:contract, binary()}) :: nil | non_neg_integer()
   def tx_index({:contract, id}) do
     pk = Validate.id!(id)
 
     case prev(Model.Origin, {:contract_create_tx, pk, <<>>}) do
-      :"$end_of_table" -> nil
-      {:contract_create_tx, ^pk, txi} -> txi
+      :"$end_of_table" ->
+        nil
+
+      {:contract_create_tx, ^pk, txi} ->
+        txi
+
+      {:contract_create_tx, _pk, _txi} ->
+        # contract_create_tx might not yet have been synced
+        nil
     end
   end
 
+  @spec pubkey({:contract, term()}) :: term()
   def pubkey({:contract, txi}) do
     case next(Model.RevOrigin, {txi, :contract_create_tx, <<>>}) do
       :"$end_of_table" -> nil
