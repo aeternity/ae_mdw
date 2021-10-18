0baa04ab997e1482a557ebf4bea14e54ef29fb16
Rogerio Pontual <rppontual@gmail.com>
2021-10-18 16:42:20 +0100

    style: fix formatting

--- test/ae_mdw/sync/async_tasks/producer_consumer_test.exs
+++ test/ae_mdw/sync/async_tasks/producer_consumer_test.exs
@@ -14,12 +14,15 @@ defmodule AeMdw.Sync.AsyncTasks.ProducerConsumerTest do
 
   @task_type :update_aex9_presence
   @contract_pk Validate.id!("ct_2bwK4mxEe3y9SazQRPXE8NdXikSTqF2T9FhNrawRzFA21yacTo")
-  @account_pk  Validate.id!("ak_2CuCtmoExsuW7iG5BbRAoncSZAoZt8Abu21qrPUhqfFNin75iL")
+  @account_pk Validate.id!("ak_2CuCtmoExsuW7iG5BbRAoncSZAoZt8Abu21qrPUhqfFNin75iL")
 
   test "enqueue, dequeue and aex9 presence update success" do
     # setup
-    exists_before? = :mnesia.async_dirty(fn -> Contract.aex9_presence_exists?(@contract_pk, @account_pk, -1) end)
+    exists_before? =
+      :mnesia.async_dirty(fn -> Contract.aex9_presence_exists?(@contract_pk, @account_pk, -1) end)
+
     if exists_before?, do: setup_delete_aex9_presence(@contract_pk, @account_pk)
+
     on_exit(fn ->
       if not exists_before?, do: setup_delete_aex9_presence(@contract_pk, @account_pk)
     end)
@@ -38,10 +41,13 @@ defmodule AeMdw.Sync.AsyncTasks.ProducerConsumerTest do
     Producer.enqueue(@task_type, args)
     Process.sleep(1000)
     assert Util.read(Model.AsyncTasks, index) == []
+
     assert :mnesia.async_dirty(fn -> Contract.aex9_presence_exists?(@contract_pk, @account_pk) end)
   end
 
   defp setup_delete_aex9_presence(contract_pk, account_pk) do
-    :mnesia.sync_dirty(fn -> :mnesia.delete(Model.Aex9AccountPresence, {account_pk, -1, contract_pk}, :delete) end)
+    :mnesia.sync_dirty(fn ->
+      :mnesia.delete(Model.Aex9AccountPresence, {account_pk, -1, contract_pk}, :delete)
+    end)
   end
 end
