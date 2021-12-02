031e5fdf52ab12a597d9b5c1cd356bb446de4347
Rogerio Pontual <rppontual@gmail.com>
2021-12-02 17:04:54 +0000

    ci: dialyzer ignore due to SmartGlobal

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -5,5 +5,6 @@
   # Following warnings are due to bad implementation of riverside.
   {"lib/phoenix/router.ex", :pattern_match, 402},
   {"lib/riverside.ex", :pattern_match, 876},
+  {"lib/ae_mdw/db/sync/transaction.ex", :pattern_match, 138},
   {"lib/ae_mdw_web/websocket/socket_handler.ex", :overlapping_contract, 2}
 ]
