72c500109cfb15afb11a17fdaafe7ef920d5a5a5
Sebastian Borrazas <seba.borrazas@gmail.com>
2021-12-09 08:25:37 -0300

    test: add stats endpoints integration tests (#383)

---
+++ test/integration/ae_mdw_web/controllers/stats_controller_test.exs
@@ -0,0 +1,205 @@
+defmodule Integration.AeMdwWeb.StatsControllerTest do
+  use AeMdwWeb.ConnCase, async: false
+
+  alias AeMdw.Db.Util
+
+  @moduletag :integration
+
+  @default_limit 10
+
+  describe "stats" do
+    test "when no subpath it gets stats in backwards direction", %{conn: conn} do
+      limit = 3
+      last_gen = Util.last_gen()
+
+      conn = get(conn, "/stats?limit=#{limit}")
+      response = json_response(conn, 200)
+
+      assert ^limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.zip(last_gen..0)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert ^limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((last_gen - limit)..(last_gen - @default_limit - 2))
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "when direction=forward it gets stats starting from 1", %{conn: conn} do
+      conn = get(conn, "/stats/forward")
+      response = json_response(conn, 200)
+
+      assert @default_limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.with_index(1)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert @default_limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((1 + @default_limit)..(10 + @default_limit))
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "it gets generations with numeric range and default limit", %{conn: conn} do
+      first = 305_000
+      last = 305_100
+      conn = get(conn, "/stats/gen/#{first}-#{last}")
+      response = json_response(conn, 200)
+
+      assert @default_limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.zip(first..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert @default_limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((first + @default_limit)..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "it gets generations backwards with numeric range and limit=1", %{conn: conn} do
+      first = 305_100
+      last = 305_000
+      limit = 1
+      conn = get(conn, "/stats/gen/#{first}-#{last}?limit=#{limit}")
+      response = json_response(conn, 200)
+
+      assert ^limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.zip(first..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert ^limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((first - limit)..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "renders error when the range is invalid", %{conn: conn} do
+      range = "invalid"
+      conn = get(conn, "/stats/gen/#{range}")
+      error_msg = "invalid range: #{range}"
+
+      assert %{"error" => ^error_msg} = json_response(conn, 400)
+    end
+  end
+
+  describe "sum_stats" do
+    test "when no subpath it gets stats in backwards direction", %{conn: conn} do
+      limit = 3
+      last_gen = Util.last_gen()
+
+      conn = get(conn, "/totalstats?limit=#{limit}")
+      response = json_response(conn, 200)
+
+      assert ^limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.zip(last_gen..0)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert ^limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((last_gen - 3)..(last_gen - 5))
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "when direction=forward it gets stats starting from 1", %{conn: conn} do
+      conn = get(conn, "/totalstats/forward")
+      response = json_response(conn, 200)
+
+      assert @default_limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.with_index(0)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert @default_limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip(10..19)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "it gets generations with numeric range and default limit", %{conn: conn} do
+      first = 305_000
+      last = 305_100
+      conn = get(conn, "/totalstats/gen/#{first}-#{last}")
+      response = json_response(conn, 200)
+
+      assert @default_limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.zip(first..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert @default_limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((first + @default_limit)..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "it gets generations backwards with numeric range and limit=1", %{conn: conn} do
+      first = 305_100
+      last = 305_000
+      limit = 1
+      conn = get(conn, "/totalstats/gen/#{first}-#{last}?limit=#{limit}")
+      response = json_response(conn, 200)
+
+      assert ^limit = Enum.count(response["data"])
+
+      assert response["data"]
+             |> Enum.zip(first..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+
+      conn_next = get(conn, response["next"])
+      response_next = json_response(conn_next, 200)
+
+      assert ^limit = Enum.count(response_next["data"])
+
+      assert response_next["data"]
+             |> Enum.zip((first - limit)..last)
+             |> Enum.all?(fn {%{"height" => height}, index} -> height == index end)
+    end
+
+    test "renders error when the range is invalid", %{conn: conn} do
+      range = "invalid"
+      conn = get(conn, "/totalstats/gen/#{range}")
+      error_msg = "invalid range: #{range}"
+
+      assert %{"error" => ^error_msg} = json_response(conn, 400)
+    end
+  end
+end
