92dc722187dbb4555b5adc895dd1c140ba2beb97
Sebastian Borrazas <seba.borrazas@gmail.com>
2021-12-12 11:33:41 -0300

    test: add contract controller endpoints integration tests

--- test/integration/ae_mdw_web/controllers/contract_controller_test.exs
+++ test/integration/ae_mdw_web/controllers/contract_controller_test.exs
@@ -2,105 +2,116 @@ defmodule Integration.AeMdwWeb.ContractControllerTest do
   use AeMdwWeb.ConnCase
   @moduletag :integration
 
-  alias AeMdwWeb.TestUtil
-
-  import AeMdw.TestDbUtil
-
-  import Assertions,
-    only: [
-      assert_maps_equal: 3
-    ]
-
   @default_limit 10
-  @second_range 11..20
 
   @contract0 "ct_2AfnEfCSZCTEkxL5Yoi4Yfq6fF7YapHRaFKDJK3THMXMBspp5z"
   @contract1 "ct_2YEtP5SNQc7NLsZZV2DE7rbK7WTdmGMuw24F3Fqu9iDAk3qpFN"
   @contract2 "ct_2QKWLinRRozwA6wPAnW269hCHpkL1vcb2YCTrna94nP7rAPVU9"
 
-  defp path(direction) when direction in [:forward, :backward] do
-    Routes.contract_path(build_conn(), :logs, direction)
-  end
-
   describe "logs" do
-    test "get events of a contract with forward path", %{conn: conn} do
-      path = path(:forward)
-      conn = get(conn, path, contract_id: @contract0)
+    test "it get logs backwards without any filters", %{conn: conn} do
+      assert %{"data" => logs, "next" => next} =
+               conn |> get("/contracts/logs") |> json_response(200)
 
-      assert response = json_response(conn, 200)
-      assert Enum.count(response["data"]) == @default_limit
-      assert Jason.encode!(response["data"]) == get_contract_logs_json(@contract0)
+      call_txis =
+        logs
+        |> Enum.map(fn %{"call_txi" => call_txi} -> call_txi end)
+        |> Enum.reverse()
 
-      conn_next = get(conn, response["next"])
-      response_next = json_response(conn_next, 200)
+      assert @default_limit = length(call_txis)
+      assert ^call_txis = Enum.sort(call_txis)
 
-      assert Enum.count(response_next["data"]) == @default_limit
+      assert %{"data" => next_logs} = conn |> get(next) |> json_response(200)
 
-      assert Jason.encode!(response_next["data"]) ==
-               get_contract_logs_json(@contract0, :forward, @second_range)
+      next_call_txis =
+        next_logs
+        |> Enum.map(fn %{"call_txi" => call_txi} -> call_txi end)
+        |> Enum.reverse()
 
-      assert response_next["data"] != response["data"]
-
-      Enum.each(response_next["data"], fn log_next ->
-        Enum.each(response["data"], fn log -> assert log_next["call_txi"] > log["call_txi"] end)
-      end)
+      assert @default_limit = length(next_call_txis)
+      assert ^next_call_txis = Enum.sort(next_call_txis)
+      assert Enum.at(call_txis, @default_limit - 1) >= Enum.at(next_call_txis, 0)
     end
 
-    test "get events of a contract with backward path", %{conn: conn} do
-      path = path(:backward)
-      conn = get(conn, path, contract_id: @contract0)
+    test "is get logs forward without any filters", %{conn: conn} do
+      assert %{"data" => logs, "next" => next} =
+               conn
+               |> get("/contracts/logs/forward")
+               |> json_response(200)
 
-      assert response = json_response(conn, 200)
-      assert Jason.encode!(response["data"]) == get_contract_logs_json(@contract0, :backward)
+      call_txis = Enum.map(logs, fn %{"call_txi" => call_txi} -> call_txi end)
 
-      conn_next = get(conn, response["next"])
-      response_next = json_response(conn_next, 200)
+      assert @default_limit = length(call_txis)
+      assert ^call_txis = Enum.sort(call_txis)
 
-      assert Enum.count(response_next["data"]) == @default_limit
+      assert %{"data" => next_logs} = conn |> get(next) |> json_response(200)
 
-      assert Jason.encode!(response_next["data"]) ==
-               get_contract_logs_json(@contract0, :backward, @second_range)
+      next_call_txis = Enum.map(next_logs, fn %{"call_txi" => call_txi} -> call_txi end)
 
-      assert response_next["data"] != response["data"]
-
-      Enum.each(response_next["data"], fn log_next ->
-        Enum.each(response["data"], fn log -> assert log_next["call_txi"] < log["call_txi"] end)
-      end)
+      assert @default_limit = length(next_call_txis)
+      assert ^next_call_txis = Enum.sort(next_call_txis)
+      assert Enum.at(call_txis, @default_limit - 1) <= Enum.at(next_call_txis, 0)
     end
 
-    test "get events from a remote contract call", %{conn: conn} do
-      path = path(:forward)
+    test "it gets logs forward belonging to a contract", %{conn: conn} do
+      contract_id = @contract0
 
-      conn = get(conn, path, contract_id: @contract2)
-      contract2_logs_json = get_contract_logs_json(@contract2)
-      assert Jason.encode!(json_response(conn, 200)["data"]) == contract2_logs_json
+      assert %{"data" => logs, "next" => next} =
+               conn
+               |> get("/contracts/logs/forward", contract_id: contract_id)
+               |> json_response(200)
+
+      create_txis = Enum.map(logs, fn %{"contract_txi" => create_txi} -> create_txi end)
+
+      assert @default_limit = length(create_txis)
+      assert ^create_txis = Enum.sort(create_txis)
+      assert Enum.all?(logs, &match?(%{"contract_id" => ^contract_id}, &1))
+
+      assert %{"data" => next_logs} = conn |> get(next) |> json_response(200)
+
+      next_create_txis = Enum.map(next_logs, fn %{"contract_txi" => create_txi} -> create_txi end)
+
+      assert @default_limit = length(next_create_txis)
+      assert ^next_create_txis = Enum.sort(next_create_txis)
+      assert Enum.all?(next_logs, &match?(%{"contract_id" => ^contract_id}, &1))
+      assert Enum.at(create_txis, @default_limit - 1) <= Enum.at(next_create_txis, 0)
+    end
+
+    test "it get logs from a remote contract call", %{conn: conn} do
+      contract_id = @contract2
+      ext_contract_id = @contract1
+
+      assert %{"data" => caller_logs, "next" => next} =
+               conn
+               |> get("/contracts/logs/forward", contract_id: contract_id)
+               |> json_response(200)
+
+      assert Enum.all?(caller_logs, &match?(%{"contract_id" => ^contract_id}, &1))
 
       # remote call logged in caller (contract2)
       assert log_in_caller =
-               contract2_logs_json
-               |> Jason.decode!()
-               |> Enum.find(fn log -> log["ext_caller_contract_id"] == @contract1 end)
+               Enum.find(
+                 caller_logs,
+                 &match?(%{"ext_caller_contract_id" => ^ext_contract_id}, &1)
+               )
 
-      conn = get(conn, path, contract_id: @contract1)
-      contract1_logs_json = get_contract_logs_json(@contract1)
-      assert Jason.encode!(json_response(conn, 200)["data"]) == contract1_logs_json
+      assert %{"data" => called_logs} =
+               conn
+               |> get("/contracts/logs/forward", contract_id: ext_contract_id)
+               |> json_response(200)
 
       # remote call logged in called (contract1)
       assert log_in_called =
-               contract1_logs_json
-               |> Jason.decode!()
-               |> Enum.find(fn log -> log["call_txi"] == log_in_caller["call_txi"] end)
+               Enum.find(called_logs, fn log -> log["call_txi"] == log_in_caller["call_txi"] end)
 
-      assert log_in_caller["contract_id"] == @contract2
-      assert log_in_called["contract_id"] == @contract1
-      assert log_in_called["parent_contract_id"] == @contract2
+      assert @contract2 = log_in_caller["contract_id"]
+      assert @contract1 = log_in_called["contract_id"]
+      assert @contract2 = log_in_called["parent_contract_id"]
 
-      assert_maps_equal(log_in_caller, log_in_called, [
-        :args,
-        :data,
-        :event_hash,
-        :call_tx_hash
-      ])
+      assert Enum.all?(
+               ~w(args data event_hash call_tx_hash),
+               &(log_in_caller[&1] == log_in_called[&1])
+             )
     end
 
     test "get events from a contract init", %{conn: conn} do
@@ -136,11 +147,12 @@ defmodule Integration.AeMdwWeb.ContractControllerTest do
 
     test "renders error when the id is invalid", %{conn: conn} do
       contract_id = "ct_NoSuchContract"
-      conn = get(conn, path(:forward), contract_id: contract_id)
+      error_msg = "invalid id: #{contract_id}"
 
-      assert json_response(conn, 400) == %{
-               "error" => TestUtil.handle_input(fn -> get_contract_logs_json(contract_id) end)
-             }
+      assert %{"error" => ^error_msg} =
+               conn
+               |> get("/contracts/logs/forward", contract_id: contract_id)
+               |> json_response(400)
     end
 
     test "when contract log doesn't have a creation txi, contract_id shouldn't be nil", %{
@@ -148,9 +160,9 @@ defmodule Integration.AeMdwWeb.ContractControllerTest do
     } do
       contract_id = "ct_eJhrbPPS4V97VLKEVbSCJFpdA4uyXiZujQyLqMFoYV88TzDe6"
 
-      assert %{"data" => contract_logs} =
+      assert %{"data" => logs} =
                conn
-               |> get(path(:forward), contract_id: contract_id)
+               |> get("/contracts/logs/forward", contract_id: contract_id)
                |> json_response(200)
 
       assert [
@@ -162,27 +174,205 @@ defmodule Integration.AeMdwWeb.ContractControllerTest do
                  "ext_caller_contract_txi" => -1
                }
                | rest
-             ] = contract_logs
+             ] = logs
 
-      assert Enum.all?(rest, fn
-               %{"contract_id" => ^contract_id} -> true
-               _contract -> false
-             end)
+      assert Enum.all?(rest, &match?(%{"contract_id" => ^contract_id}, &1))
+    end
+
+    test "it returns the called filtered by event hash", %{conn: conn} do
+      event = "TipReceived"
+      event_hash = event |> :aec_hash.blake2b_256_hash() |> Base.hex_encode32()
+
+      assert %{"data" => logs, "next" => next} =
+               conn
+               |> get("/contracts/logs", event: event)
+               |> json_response(200)
+
+      call_txis = logs |> Enum.map(fn %{"call_txi" => call_txi} -> call_txi end) |> Enum.reverse()
+
+      assert @default_limit = length(call_txis)
+      assert ^call_txis = Enum.sort(call_txis)
+      assert Enum.all?(logs, &match?(%{"event_hash" => ^event_hash}, &1))
+
+      assert %{"data" => next_logs} = conn |> get(next) |> json_response(200)
+
+      next_call_txis =
+        next_logs |> Enum.map(fn %{"call_txi" => call_txi} -> call_txi end) |> Enum.reverse()
+
+      assert @default_limit = length(next_call_txis)
+      assert ^next_call_txis = Enum.sort(next_call_txis)
+      assert Enum.at(call_txis, @default_limit - 1) >= Enum.at(next_call_txis, 0)
+    end
+
+    test "it returns calls filtered by data prefix", %{conn: conn} do
+      data_prefix = "aeternity.com"
+
+      assert %{"data" => logs, "next" => next} =
+               conn
+               |> get("/contracts/logs", data: URI.encode(data_prefix))
+               |> json_response(200)
+
+      data_call_txis =
+        logs
+        |> Enum.map(fn %{"data" => data, "call_txi" => call_txi} -> {data, call_txi} end)
+        |> Enum.reverse()
+
+      assert length(data_call_txis) > 0
+      assert ^data_call_txis = Enum.sort(data_call_txis)
+      assert Enum.all?(logs, fn %{"data" => data} -> String.starts_with?(data, data_prefix) end)
     end
   end
 
   describe "calls" do
-    test "it gets calls from a contract", %{conn: conn} do
-      contract_id = "ct_2uJthb5s1D8c8F8ZYMAZ6LYGWno5ubFnrmkkHLE1FBzN3JruQw"
+    test "it get logs backwards without any filters", %{conn: conn} do
+      assert %{"data" => calls, "next" => next} =
+               conn |> get("/contracts/calls") |> json_response(200)
 
-      assert %{"data" => calls} =
+      call_txis =
+        calls
+        |> Enum.map(fn %{"call_txi" => call_txi} -> call_txi end)
+        |> Enum.reverse()
+
+      assert @default_limit = length(call_txis)
+      assert ^call_txis = Enum.sort(call_txis)
+      assert %{"data" => next_calls} = conn |> get(next) |> json_response(200)
+
+      next_call_txis =
+        next_calls |> Enum.map(fn %{"call_txi" => call_txi} -> call_txi end) |> Enum.reverse()
+
+      assert @default_limit = length(next_call_txis)
+      assert ^next_call_txis = Enum.sort(next_call_txis)
+      assert Enum.at(call_txis, @default_limit - 1) >= Enum.at(next_call_txis, 0)
+    end
+
+    test "is get calls forward without any filters", %{conn: conn} do
+      assert %{"data" => calls, "next" => next} =
                conn
-               |> get("/contracts/calls/forward?contract_id=#{contract_id}")
+               |> get("/contracts/calls/forward")
                |> json_response(200)
 
-      assert 10 = length(calls)
+      call_txis = Enum.map(calls, fn %{"call_txi" => call_txi} -> call_txi end)
+
+      assert @default_limit = length(call_txis)
+      assert ^call_txis = Enum.sort(call_txis)
+      assert %{"data" => next_calls} = conn |> get(next) |> json_response(200)
+
+      next_call_txis = Enum.map(next_calls, fn %{"call_txi" => call_txi} -> call_txi end)
+
+      assert @default_limit = length(next_call_txis)
+      assert ^next_call_txis = Enum.sort(next_call_txis)
+      assert Enum.at(call_txis, @default_limit - 1) <= Enum.at(next_call_txis, 0)
+    end
+
+    test "it gets calls forward belonging to a contract", %{conn: conn} do
+      contract_id = "ct_2uJthb5s1D8c8F8ZYMAZ6LYGWno5ubFnrmkkHLE1FBzN3JruQw"
+
+      assert %{"data" => calls, "next" => next} =
+               conn
+               |> get("/contracts/calls/forward", contract_id: contract_id)
+               |> json_response(200)
+
+      create_txis = Enum.map(calls, fn %{"contract_txi" => create_txi} -> create_txi end)
+
+      assert @default_limit = length(create_txis)
+      assert ^create_txis = Enum.sort(create_txis)
       assert %{"internal_tx" => %{"query" => query_b64}} = Enum.at(calls, 2)
       assert {:ok, _query} = Base.decode64(query_b64)
+
+      %{"data" => next_calls} = conn |> get(next) |> json_response(200)
+
+      next_create_txis =
+        Enum.map(next_calls, fn %{"contract_txi" => create_txi} -> create_txi end)
+
+      assert @default_limit = length(next_create_txis)
+      assert ^next_create_txis = Enum.sort(next_create_txis)
+      assert Enum.at(create_txis, @default_limit - 1) <= Enum.at(next_create_txis, 0)
+    end
+
+    test "it gets calls forward filtered by a function name prefix", %{conn: conn} do
+      fname_prefix = "Oracle"
+
+      assert %{"data" => calls, "next" => next} =
+               conn
+               |> get("/contracts/calls/forward", function: fname_prefix)
+               |> json_response(200)
+
+      fnames = Enum.map(calls, fn %{"function" => fname} -> fname end)
+
+      assert @default_limit = length(fnames)
+      assert ^fnames = Enum.sort(fnames)
+
+      %{"data" => next_calls} = conn |> get(next) |> json_response(200)
+
+      next_fnames = Enum.map(next_calls, fn %{"function" => fname} -> fname end)
+
+      assert @default_limit = length(next_fnames)
+      assert ^next_fnames = Enum.sort(next_fnames)
+      assert Enum.at(fnames, @default_limit - 1) <= Enum.at(next_fnames, 0)
+    end
+
+    test "it gets calls forward filtered by recipient_id", %{conn: conn} do
+      recipient_id = "ak_23bfFKQ1vuLeMxyJuCrMHiaGg5wc7bAobKNuDadf8tVZUisKWs"
+
+      assert %{"data" => calls, "next" => next} =
+               conn
+               |> get("/contracts/calls/forward", recipient_id: recipient_id)
+               |> json_response(200)
+
+      call_txi_local_idxs =
+        Enum.map(calls, fn %{"call_txi" => call_txi, "local_idx" => local_idx} ->
+          {call_txi, local_idx}
+        end)
+
+      assert @default_limit = length(call_txi_local_idxs)
+      assert ^call_txi_local_idxs = Enum.sort(call_txi_local_idxs)
+      assert Enum.all?(calls, &match?(%{"internal_tx" => %{"recipient_id" => ^recipient_id}}, &1))
+
+      %{"data" => next_calls} = conn |> get(next) |> json_response(200)
+
+      next_call_txi_local_idxs =
+        Enum.map(next_calls, fn %{"call_txi" => call_txi, "local_idx" => local_idx} ->
+          {call_txi, local_idx}
+        end)
+
+      assert @default_limit = length(next_call_txi_local_idxs)
+      assert ^next_call_txi_local_idxs = Enum.sort(next_call_txi_local_idxs)
+
+      assert Enum.at(call_txi_local_idxs, @default_limit - 1) <=
+               Enum.at(next_call_txi_local_idxs, 0)
+    end
+
+    test "it gets calls filtered by recipient_id and contract_id", %{conn: conn} do
+      contract_id = "ct_2AfnEfCSZCTEkxL5Yoi4Yfq6fF7YapHRaFKDJK3THMXMBspp5z"
+      recipient_id = "ak_23bfFKQ1vuLeMxyJuCrMHiaGg5wc7bAobKNuDadf8tVZUisKWs"
+
+      assert %{"data" => calls, "next" => next} =
+               conn
+               |> get("/contracts/calls/forward", recipient_id: recipient_id)
+               |> json_response(200)
+
+      call_txi_local_idxs =
+        Enum.map(calls, fn %{"call_txi" => call_txi, "local_idx" => local_idx} ->
+          {call_txi, local_idx}
+        end)
+
+      assert @default_limit = length(call_txi_local_idxs)
+      assert ^call_txi_local_idxs = Enum.sort(call_txi_local_idxs)
+      assert Enum.all?(calls, &match?(%{"internal_tx" => %{"recipient_id" => ^recipient_id}}, &1))
+      assert Enum.all?(calls, &match?(%{"contract_id" => ^contract_id}, &1))
+
+      %{"data" => next_calls} = conn |> get(next) |> json_response(200)
+
+      next_call_txi_local_idxs =
+        Enum.map(next_calls, fn %{"call_txi" => call_txi, "local_idx" => local_idx} ->
+          {call_txi, local_idx}
+        end)
+
+      assert @default_limit = length(next_call_txi_local_idxs)
+      assert ^next_call_txi_local_idxs = Enum.sort(next_call_txi_local_idxs)
+
+      assert Enum.at(call_txi_local_idxs, @default_limit - 1) <=
+               Enum.at(next_call_txi_local_idxs, 0)
     end
   end
 end
