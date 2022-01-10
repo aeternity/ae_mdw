39c24c82077b6f62f838715b209b611ddc8c19b6
Rogerio Pontual <rppontual@gmail.com>
2022-01-10 13:06:44 +0000

    test: fix auction sorting check

--- test/integration/ae_mdw_web/controllers/name_controller_test.exs
+++ test/integration/ae_mdw_web/controllers/name_controller_test.exs
@@ -295,10 +295,13 @@ defmodule Integration.AeMdwWeb.NameControllerTest do
       assert %{"data" => auctions} =
                conn |> get("/names/auctions", limit: limit) |> json_response(200)
 
-      plain_names = Enum.map(auctions, fn %{"name" => plain_name} -> plain_name end)
+      plain_names =
+        Enum.map(auctions, fn %{"name" => plain_name, "info" => %{"auction_end" => auction_end}} ->
+          {plain_name, auction_end}
+        end)
 
       assert length(auctions) <= limit
-      assert ^plain_names = Enum.sort(plain_names)
+      assert ^plain_names = Enum.sort_by(plain_names, fn {_name, expire} -> expire end, :desc)
     end
 
     test "get auctions with parameters by=expiration, direction=forward and limit=3", %{
