6f69e2d7c4cc223256561c158ffcef081ac4bd6c
Sebastian Borrazas <seba.borrazas@gmail.com>
2022-02-11 13:19:58 -0300

    fix: skip rev parameter processing for contract endpoints

--- lib/ae_mdw/contracts.ex
+++ lib/ae_mdw/contracts.ex
@@ -44,7 +44,7 @@ defmodule AeMdw.Contracts do
   @id_int_contract_call_table Model.IdIntContractCall
   @grp_id_int_contract_call_table Model.GrpIdIntContractCall
 
-  @pagination_params ~w(limit cursor)
+  @pagination_params ~w(limit cursor rev)
 
   @max_256bit_int Util.max_256bit_int()
   @max_256bit_bin Util.max_256bit_bin()
