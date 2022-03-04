4c60302e357f8e046dd41701eb4fc4ed24784566
Rogerio Pontual <44991200+jyeshe@users.noreply.github.com>
2022-03-04 09:10:05 +0000

    fix: /stats counters with negative values (#562)

--- lib/ae_mdw/stats.ex
+++ lib/ae_mdw/stats.ex
@@ -11,6 +11,7 @@ defmodule AeMdw.Stats do
 
   require Model
 
+  @type stat() :: map()
   @type delta_stat() :: map()
   @type total_stat() :: map()
   @type cursor() :: binary() | nil
@@ -22,6 +23,30 @@ defmodule AeMdw.Stats do
 
   @delta_table Model.DeltaStat
   @totals_table Model.TotalStat
+
+  # Legacy v1 is a blending between /totalstats and /deltastats.
+  # The active and inactive object counters are totals while the rewards are delta.
+  @spec fetch_stats_v1(direction(), range(), cursor(), limit()) ::
+          {cursor(), [stat()], cursor()}
+  def fetch_stats_v1(direction, range, cursor, limit) do
+    {:ok, last_gen} = Database.last_key(AeMdw.Db.Model.TotalStat)
+
+    {range_first, range_last} =
+      case range do
+        nil -> {1, last_gen}
+        {:gen, %Range{first: first, last: last}} -> {max(first, 1), min(last, last_gen)}
+      end
+
+    cursor = deserialize_cursor(cursor)
+
+    case Util.build_gen_pagination(cursor, direction, range_first, range_last, limit) do
+      {:ok, prev_cursor, range, next_cursor} ->
+        {serialize_cursor(prev_cursor), render_stats(range), serialize_cursor(next_cursor)}
+
+      :error ->
+        {nil, [], nil}
+    end
+  end
 
   @spec fetch_delta_stats(direction(), range(), cursor(), limit()) ::
           {cursor(), [delta_stat()], cursor()}
@@ -38,7 +63,7 @@ defmodule AeMdw.Stats do
 
     case Util.build_gen_pagination(cursor, direction, range_first, range_last, limit) do
       {:ok, prev_cursor, range, next_cursor} ->
-        {serialize_cursor(prev_cursor), render_stats(range), serialize_cursor(next_cursor)}
+        {serialize_cursor(prev_cursor), render_delta_stats(range), serialize_cursor(next_cursor)}
 
       :error ->
         {nil, [], nil}
@@ -67,17 +92,47 @@ defmodule AeMdw.Stats do
     end
   end
 
-  @spec fetch_stat!(height()) :: delta_stat()
-  def fetch_stat!(height), do: render_stat(Database.fetch!(@delta_table, height))
+  @spec fetch_delta_stat!(height()) :: delta_stat()
+  def fetch_delta_stat!(height), do: render_delta_stat(Database.fetch!(@delta_table, height))
 
   @spec fetch_total_stat!(height()) :: total_stat()
   def fetch_total_stat!(height), do: render_total_stat(Database.fetch!(@totals_table, height))
 
-  defp render_stats(gens), do: Enum.map(gens, &fetch_stat!/1)
+  defp render_stats(%Range{first: first, last: last}) do
+    Enum.map(first..last, fn height ->
+      %{
+        block_reward: block_reward,
+        dev_reward: dev_reward
+      } = fetch_delta_stat!(height - 1)
+
+      %{
+        active_auctions: active_auctions,
+        active_names: active_names,
+        inactive_names: inactive_names,
+        active_oracles: active_oracles,
+        inactive_oracles: inactive_oracles,
+        contracts: contracts
+      } = fetch_total_stat!(height)
+
+      %{
+        height: height,
+        block_reward: block_reward,
+        dev_reward: dev_reward,
+        active_auctions: active_auctions,
+        active_names: active_names,
+        inactive_names: inactive_names,
+        active_oracles: active_oracles,
+        inactive_oracles: inactive_oracles,
+        contracts: contracts
+      }
+    end)
+  end
+
+  defp render_delta_stats(gens), do: Enum.map(gens, &fetch_delta_stat!/1)
 
   defp render_total_stats(gens), do: Enum.map(gens, &fetch_total_stat!/1)
 
-  defp render_stat(stat), do: Format.to_map(stat, @delta_table)
+  defp render_delta_stat(stat), do: Format.to_map(stat, @delta_table)
 
   defp render_total_stat(total_stat), do: Format.to_map(total_stat, @totals_table)
 
