5b37ad2dba507c61c88e0d8809695f4401c9fe3d
Rogerio Pontual <rppontual@gmail.com>
2022-03-10 20:01:04 +0000

    test: cleanup async tasks on stats

--- test/ae_mdw/sync/async_tasks/stats_test.exs
+++ test/ae_mdw/sync/async_tasks/stats_test.exs
@@ -23,29 +23,26 @@ defmodule AeMdw.Sync.AsyncTasks.StatsTest do
         [
           "ct_M9yohHgcLjhpp1Z8SaA1UTmRMQzR4FWjJHajGga8KBoZTEPwC",
           "ct_6ZuwbMgcNDaryXTnrLMiPFW2ogE9jxAzz1874BToE81ksWek6",
-          "ct_2M2dJU2wLWPE73HpLPmFezqqJbu9PZ8rwKxeDvrids4y1nPYA2",
-          "ct_X8sNFxiae4xmEdrzipFAQSaHnC3jwBhiPo51Le4JmDw1bMhSF",
-          "ct_wevncvzDCpbG28Myuq2Ec48CLqte1vJPFdwTxtiYPJFJ9cZEe",
-          "ct_2vSP4tmmxuTpkLzvxN2oU3MpS2yCSRYwsXBSyGWLY6Pcw116Dz",
-          "ct_v86iyqGjPGDQGtgH2wqLfzz13k4kGxpsoJwZMqgCiLxP3Wb97",
-          "ct_2iC3WZQLgPshQm4wUDgnKyz1YaDjARcT7VfowZPaqMrkKELUR1",
-          "ct_x4ybvRpo1fXXEVWtcxyawzqbh6vdMyB4Srb6VSCZfrMAfRobD",
-          "ct_2L2EDASxVEoMW7praTCsAVJS1Ar7khqDn1WTzkYxKPTFqGMaiL"
+          "ct_2M2dJU2wLWPE73HpLPmFezqqJbu9PZ8rwKxeDvrids4y1nPYA2"
         ]
         |> Enum.map(&AeMdw.Validate.id!/1)
+
+      on_exit(fn ->
+        Enum.each(ct_pks, &setup_delete_async_task/1)
+      end)
 
       pending_count = length(ct_pks)
 
       # setup new to expected pending
-      Enum.each(1..pending_count, fn i ->
-        index = {System.system_time() + i, :update_aex9_presence}
-        m_task = Model.async_tasks(index: index, args: [Enum.at(ct_pks, i - 1)])
+      Enum.each(ct_pks, fn ct_pk ->
+        index = {System.system_time(), :update_aex9_presence}
+        m_task = Model.async_tasks(index: index, args: [ct_pk])
         Database.dirty_write(Model.AsyncTasks, m_task)
       end)
 
       assert %{producer_buffer: 0, total_pending: 0} = Stats.counters()
-      assert :ok = Stats.update_buffer_len(10, 100)
-      assert %{producer_buffer: 10, total_pending: ^pending_count} = Stats.counters()
+      assert :ok = Stats.update_buffer_len(5, 100)
+      assert %{producer_buffer: 5, total_pending: ^pending_count} = Stats.counters()
     end
   end
 
@@ -58,5 +55,15 @@ defmodule AeMdw.Sync.AsyncTasks.StatsTest do
       assert :ok = Stats.update_consumed(true)
       assert %{producer_buffer: 15, long_tasks: 1} = Stats.counters()
     end
+  end
+
+  defp setup_delete_async_task(ct_pk) do
+    Model.async_tasks(index: key) =
+      Model.AsyncTasks
+      |> Database.all_keys()
+      |> Enum.map(&Database.fetch!(Model.AsyncTasks, &1))
+      |> Enum.find(fn m_task -> Model.async_tasks(m_task, :args) == [ct_pk] end)
+
+    Database.dirty_delete(Model.AsyncTasks, key)
   end
 end
