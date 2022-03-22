8688c453eb367469be0390cbd6109638741425b3
Rogerio Pontual <rppontual@gmail.com>
2022-03-22 10:06:15 +0000

    test: /txs cases without scope and with direction

--- test/ae_mdw_web/controllers/tx_controller_test.exs
+++ test/ae_mdw_web/controllers/tx_controller_test.exs
@@ -6,7 +6,7 @@ defmodule AeMdwWeb.TxControllerTest do
   alias AeMdw.Db.Util
 
   describe "txs" do
-    test "it returns 400 when no direction specified", %{conn: conn} do
+    test "without scope returns 200 and response data", %{conn: conn} do
       with_mocks [
         {Util, [],
          [
@@ -14,7 +14,19 @@ defmodule AeMdwWeb.TxControllerTest do
            last_gen: fn -> 1_000 end
          ]}
       ] do
-        assert %{"error" => "no such route"} = conn |> get("/txs") |> json_response(400)
+        assert %{"data" => []} = conn |> get("/txs") |> json_response(200)
+      end
+    end
+
+    test "with direction returns 200 and response data", %{conn: conn} do
+      with_mocks [
+        {Util, [],
+         [
+           first_gen: fn -> 0 end,
+           last_gen: fn -> 1_000 end
+         ]}
+      ] do
+        assert %{"data" => []} = conn |> get("/txs?direction=forward") |> json_response(200)
       end
     end
   end
