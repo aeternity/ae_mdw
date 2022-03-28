da4770eb2c04453212190c1a3d0d601e2dcd4747
Rogerio Pontual <rppontual@gmail.com>
2022-03-28 15:33:05 +0100

    test: start producer and consumer for integration test

--- test/integration/ae_mdw/sync/async_tasks/producer_consumer_test.exs
+++ test/integration/ae_mdw/sync/async_tasks/producer_consumer_test.exs
@@ -3,10 +3,10 @@ defmodule Integration.AeMdw.Sync.AsyncTasks.ProducerConsumerTest do
 
   @moduletag :integration
 
+  alias AeMdw.Database
   alias AeMdw.Db.Contract
   alias AeMdw.Db.Model
   alias AeMdw.Db.Origin
-  alias AeMdw.Db.Util
   alias AeMdw.Sync.AsyncTasks.Producer
   alias AeMdw.Sync.AsyncTasks
   alias AeMdw.Validate
@@ -41,49 +41,26 @@ defmodule Integration.AeMdw.Sync.AsyncTasks.ProducerConsumerTest do
 
     # check async enqueue and sync dequeue
     args = [@contract_pk]
-    Producer.enqueue(@task_type, args)
-    Producer.enqueue(@task_type, args)
-    Producer.commit_enqueued()
-
-    assert index =
-             Enum.reduce_while(1..50, nil, fn _i, nil ->
-               Process.sleep(20)
-               task = Producer.dequeue()
-
-               if task do
-                 assert Model.async_tasks(index: index, args: ^args) = task
-                 assert [task] == Util.read(Model.AsyncTasks, index)
-
-                 {:halt, index}
-               else
-                 {:cont, nil}
-               end
-             end)
-
-    # discard task as if processed
-    Producer.notify_consumed(index, args, false)
-    assert nil == Producer.dequeue()
-
-    # enqueue and check that the async task was processed
+    AsyncTasks.Supervisor.start_link([])
     Producer.enqueue(@task_type, args)
     Producer.commit_enqueued()
 
-    [{_id, pid, _type, _mod}] =
-      AsyncTasks.Supervisor
-      |> Supervisor.which_children()
-      |> Enum.filter(fn {id, _pid, _type, _mod} ->
-        id == "Elixir.AeMdw.Sync.AsyncTasks.Consumer1"
-      end)
+    AsyncTasks.Supervisor
+    |> Supervisor.which_children()
+    |> Enum.filter(fn {id, _pid, _type, _mod} ->
+      is_binary(id) and String.starts_with?(id, "Elixir.AeMdw.Sync.AsyncTasks.Consumer")
+    end)
+    |> Enum.each(fn {_id, consumer_pid, _type, _mod} ->
+      Process.send(consumer_pid, :demand, [:noconnect])
+    end)
 
     assert Enum.reduce_while(1..50, false, fn _i, _acc ->
-             Process.send(pid, :demand, [:noconnect])
-             Process.sleep(100)
+      Process.sleep(100)
 
-             if Contract.aex9_presence_exists?(@contract_pk, @account_pk, create_txi) do
-               {:halt, true}
-             else
-               {:cont, false}
-             end
-           end)
-  end
+      if Contract.aex9_presence_exists?(@contract_pk, @account_pk, create_txi) do
+        {:halt, true}
+      else
+        {:cont, false}
+      end
+    end)  end
 end
