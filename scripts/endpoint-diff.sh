b238c0fe2dd25994e9879a30d71f4dcfb37d47f6
Sebastian Borrazas <seba.borrazas@gmail.com>
2022-04-12 13:31:05 -0300

    chore: add diffing script to compare two different environments (#628)
    Usage: `./scripts/endpoint-diff.sh URL1 URL2`
    
    By default it will compare `https://mainnet.aeternity.io` and
    `http://localhost:4000`.

---
+++ scripts/endpoint-diff.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+URL_1="${1:-https://mainnet.aeternity.io/mdw/}"
+URL_2="${2:-http://localhost:4000/}"
+
+ERROR=0
+SUCCESS=0
+
+mkdir -p /tmp
+
+echo "Diffing $URL_1 and $URL_2 ..."
+
+while read LINE; do
+  if [[ $LINE =~ mdw\/(.*?)\" ]]; then
+    ROUTE="${BASH_REMATCH[1]}"
+
+    echo "Diffing '$ROUTE'"
+
+    curl -so '.endpoint-diff.json1.tmp' "$URL_1$ROUTE"
+    curl -so '.endpoint-diff.json2.tmp' "$URL_2$ROUTE"
+
+    if diff .endpoint-diff.json1.tmp .endpoint-diff.json2.tmp >>/tmp/endpoint-diff.output; then
+        ((SUCCESS=SUCCESS + 1))
+    else
+        ((ERROR=ERROR + 1))
+    fi
+  fi
+done < README.md
+
+rm -f .endpoint-diff.json1.tmp .endpoint-diff.json2.tmp
+
+echo "success $SUCCESS"
+echo "error $ERROR"
