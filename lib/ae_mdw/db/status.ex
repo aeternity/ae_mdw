aeternity/ae_mdw

commit 32c25753cd74ff93d4e03515cb65a36dc1eccf7b

refactor: perform async invalidations on a sync server (#589)
This commit contains several fixes and improvements:

* Rename the `AeMdw.Db.Sync` module to `AeMdw.Sync.Server`, since
  it contains the functionality for dealing with the syncing process.
* Merge the AeMdw.Db.ForkDetection server module into
  `AeMdw.Sync.Server`, since there was no need for it to be a separate
  process.
* Synchronously deal with syncing and invalidating, since the syncing
  process is now synchronously accepting all types of messages (which
  was probably causing issues when doing it asynchronously).
* Add documentation for how to deal with blocks when syncing.
* Allow micro-block level syncing to be more straightforward.

An important aspect as well is that everthing under the
`AeMdw.Db.Sync.*` namespace contains exclusively syncing logic,
without having to deal with processes and forks. In addition, the
syncing modules dependency is a tree-like struture: Block ->
Transaction -> *.

lib/ae_mdw/db/status.ex
@@ -14,7 +14,7 @@ defmodule AeMdw.Db.Status do
     {node_syncing?, node_progress} = :aec_sync.sync_progress()
     node_height = :aec_blocks.height(top_kb)
     {mdw_tx_index, mdw_height} = safe_mdw_tx_index_and_height()
-    mdw_syncing? = is_pid(Process.whereis(AeMdw.Db.Sync.Supervisor))
+    mdw_syncing? = is_pid(Process.whereis(AeMdw.Sync.Server))
     {:ok, version} = :application.get_key(:ae_mdw, :vsn)
     async_tasks_counters = Stats.counters()
 

lib/ae_mdw/sync/server.ex
@@ -1,0 +1,128 @@
+defmodule AeMdw.Sync.Server do
+  @moduledoc """
+  Subscribes to chain events for dealing with new blocks and block
+  invalidations.
+
+  Internally keeps track of the generations that have been synced.
+  """
+
+  alias AeMdw.Db.Model
+  alias AeMdw.Db.Sync.Block
+  alias AeMdw.Db.Sync.Invalidate
+  alias AeMdw.Log
+
+  require Model
+
+  use GenServer
+
+  defstruct [:sync_pid, :fork_height, :current_height]
+
+  @unsynced_gens 1
+  @max_blocks_sync 600
+
+  @spec start_link(GenServer.options()) :: GenServer.on_start()
+  def start_link(_opts),
+    do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+
+  @spec start_sync() :: :ok
+  def start_sync, do: GenServer.cast(__MODULE__, :start_sync)
+
+  @impl true
+  def init([]) do
+    current_height = Block.synced_height()
+
+    {:ok, %__MODULE__{current_height: current_height}}
+  end
+
+  @impl true
+  def handle_cast(:start_sync, state) do
+    :aec_events.subscribe(:chain)
+    :aec_events.subscribe(:top_changed)
+
+    {:noreply, process_state(state)}
+  end
+
+  def handle_cast({:done, height}, state) do
+    new_state = process_state(%__MODULE__{state | current_height: height, sync_pid: nil})
+
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_info(
+        {:gproc_ps_event, :top_changed, %{info: %{block_type: :key, height: height}}},
+        %__MODULE__{fork_height: old_fork_height} = s
+      ) do
+    header_candidates =
+      height
+      |> :aec_db.find_headers_at_height()
+      |> Enum.filter(&(:aec_headers.type(&1) == :key))
+
+    new_state =
+      if length(header_candidates) > 1 do
+        # FORK!
+        main_header =
+          Enum.find_value(header_candidates, fn header ->
+            {:ok, header_hash} = :aec_headers.hash_header(header)
+
+            # COSTLY!
+            :aec_chain_state.hash_is_in_main_chain(header_hash) && header
+          end)
+
+        new_fork_height = :aec_headers.height(main_header)
+
+        fork_height =
+          (old_fork_height && min(old_fork_height, new_fork_height)) || new_fork_height
+
+        %__MODULE__{s | fork_height: fork_height}
+      else
+        s
+      end
+
+    {:noreply, process_state(new_state)}
+  end
+
+  def handle_info({:gproc_ps_event, :top_changed, %{info: %{block_type: :micro}}}, s),
+    do: {:noreply, s}
+
+  defp process_state(
+         %__MODULE__{current_height: current_height, sync_pid: nil, fork_height: fork_height} = s
+       ) do
+    top_height = height() - @unsynced_gens
+
+    sync_pid =
+      cond do
+        not is_nil(fork_height) and fork_height <= current_height ->
+          spawn_invalidate(fork_height)
+
+        current_height < top_height ->
+          spawn_sync(current_height + 1, min(current_height + @max_blocks_sync, top_height))
+
+        true ->
+          nil
+      end
+
+    %__MODULE__{s | sync_pid: sync_pid, fork_height: nil}
+  end
+
+  defp process_state(s), do: s
+
+  defp spawn_sync(from_height, to_height) do
+    spawn_link(fn ->
+      Block.sync(from_height, to_height)
+
+      GenServer.cast(__MODULE__, {:done, to_height})
+    end)
+  end
+
+  defp spawn_invalidate(height) do
+    spawn_link(fn ->
+      Log.info("invalidation #{height}")
+      Invalidate.invalidate(height)
+
+      GenServer.cast(__MODULE__, {:done, height - 1})
+    end)
+  end
+
+  defp height, do: :aec_headers.height(:aec_chain.top_header())
+end

lib/ae_mdw/application.ex
@@ -10,7 +10,7 @@ defmodule AeMdw.Application do
   """
   alias AeMdw.Contract
   alias AeMdw.Db.Model
-  alias AeMdw.Db.Sync
+  alias AeMdw.Sync.Server
   alias AeMdw.EtsCache
   alias AeMdw.Extract
   alias AeMdw.NodeHelper
@@ -47,11 +47,7 @@ defmodule AeMdw.Application do
 
     children =
       if Application.fetch_env!(:ae_mdw, :sync) do
-        [
-          AeMdw.Sync.AsyncTasks.Supervisor,
-          AeMdw.Db.Sync.Supervisor
-          | children
-        ]
+        [AeMdw.Sync.AsyncTasks.Supervisor, Server | children]
       else
         children
       end
@@ -242,7 +238,7 @@ defmodule AeMdw.Application do
   end
 
   def start_phase(:start_sync, _start_type, []) do
-    Sync.start_sync()
+    Server.start_sync()
   end
 
   @impl Application

lib/ae_mdw/db/model.ex
@@ -33,8 +33,6 @@ defmodule AeMdw.Db.Model do
 
   # txs block index :
   #     index = {kb_index (0..), mb_index}, tx_index = tx_index, hash = block (header) hash
-  #     if tx_index == nil -> txs not synced yet on that height
-  #     if tx_index == -1  -> no tx occured yet
   #     On keyblock boundary: mb_index = -1}
   @block_defaults [index: {-1, -1}, tx_index: nil, hash: <<>>]
   defrecord :block, @block_defaults
@@ -42,7 +40,7 @@ defmodule AeMdw.Db.Model do
   @type block ::
           record(:block,
             index: Blocks.block_index(),
-            tx_index: Txs.txi() | nil | -1,
+            tx_index: Txs.txi(),
             hash: Blocks.block_hash()
           )
 

lib/ae_mdw/db/mutations/key_blocks_mutation.ex
@@ -1,32 +1,0 @@
-defmodule AeMdw.Db.KeyBlocksMutation do
-  @moduledoc """
-  Writes key block full model for the current height and next_txi for next height.
-  """
-  alias AeMdw.Db.Model
-  alias AeMdw.Database
-  alias AeMdw.Txs
-
-  require Model
-
-  @derive AeMdw.Db.TxnMutation
-  defstruct [:key_block, :next_txi]
-
-  @opaque t() :: %__MODULE__{
-            key_block: Database.record(),
-            next_txi: Txs.txi()
-          }
-
-  @spec new(Database.record(), Txs.txi()) :: t()
-  def new(m_block, next_txi) do
-    %__MODULE__{key_block: m_block, next_txi: next_txi}
-  end
-
-  @spec execute(t(), Database.transaction()) :: :ok
-  def execute(%__MODULE__{key_block: m_block, next_txi: next_txi}, txn) do
-    {height, -1} = Model.block(m_block, :index)
-    [next_kb] = Database.read(Model.Block, {height + 1, -1})
-
-    Database.write(txn, Model.Block, m_block)
-    Database.write(txn, Model.Block, Model.block(next_kb, tx_index: next_txi))
-  end
-end

lib/ae_mdw/db/sync/block.ex
@@ -1,0 +1,151 @@
+defmodule AeMdw.Db.Sync.Block do
+  @moduledoc """
+  Syncs all blocks and transactions contained in them from a given range of
+  heights.
+
+  The steps for syncing a range of generations is:
+
+    1. Retrieve all of the key block hashes belonging to that range.
+    2. Create a key block in the Model.Block table for each of those
+       blocks.
+    3. For each key block:
+       3.1. Retrieve the key block micro blocks from chain.
+       3.2. Skip processing the microblocks that were already created,
+            get the latest txi from them.
+       3.3. Get the mutations from each micro-block and execute them.
+  """
+
+  alias AeMdw.Blocks
+  alias AeMdw.Db.Model
+  alias AeMdw.Database
+  alias AeMdw.Db.IntTransfer
+  alias AeMdw.Db.NamesExpirationMutation
+  alias AeMdw.Db.OraclesExpirationMutation
+  alias AeMdw.Db.StatsMutation
+  alias AeMdw.Db.Sync.Transaction
+  alias AeMdw.Db.WriteTxnMutation
+  alias AeMdw.Log
+  alias AeMdw.Node, as: AE
+  alias AeMdw.Sync.AsyncTasks.Producer
+  alias AeMdwWeb.Websocket.Broadcaster
+
+  require Model
+  require Logger
+
+  @log_freq 10_000
+
+  ################################################################################
+
+  @spec sync(Blocks.height(), Blocks.height()) :: :ok
+  def sync(from_height, to_height) do
+    {:ok, header} = :aec_chain.get_key_header_by_height(to_height + 1)
+    {:ok, initial_hash} = :aec_headers.hash_header(header)
+
+    {heights_hashes, _prev_hash} =
+      Enum.map_reduce((to_height + 1)..from_height, initial_hash, fn height, hash ->
+        if rem(height, @log_freq) == 0, do: Log.info("syncing block index at #{height}")
+
+        {:ok, kh} = :aec_chain.get_header(hash)
+        ^height = :aec_headers.height(kh)
+        :key = :aec_headers.type(kh)
+
+        {{height, hash}, :aec_headers.prev_key_hash(kh)}
+      end)
+
+    heights_hashes = Enum.reverse(heights_hashes)
+
+    heights_hashes
+    |> Enum.drop(if from_height == 0, do: 0, else: 1)
+    |> Enum.map(fn {height, hash} ->
+      kb_model = Model.block(index: {height, -1}, hash: hash, tx_index: 0)
+
+      WriteTxnMutation.new(Model.Block, kb_model)
+    end)
+    |> Database.commit()
+
+    heights_hashes
+    |> Enum.zip(Enum.drop(heights_hashes, 1))
+    |> Enum.each(fn {{height, kb_hash}, {_next_height, next_kb_hash}} ->
+      if rem(height, @log_freq) == 0, do: Log.info("syncing transactions at generation #{height}")
+
+      sync_generation(height, kb_hash, next_kb_hash)
+    end)
+  end
+
+  defp sync_generation(height, kb_hash, next_kb_hash) do
+    {key_block, micro_blocks} = AE.Db.get_blocks(kb_hash, next_kb_hash)
+    kb_header = :aec_blocks.to_key_header(key_block)
+
+    {:ok, {^height, last_mbi}} = Database.prev_key(Model.Block, {height + 1, -1})
+    last_txi = Database.last_key(Model.Tx, -1) + 1
+
+    :ets.delete_all_objects(:stat_sync_cache)
+    :ets.delete_all_objects(:ct_create_sync_cache)
+    :ets.delete_all_objects(:tx_sync_cache)
+
+    next_txi =
+      micro_blocks
+      |> Enum.with_index()
+      |> Enum.drop(last_mbi + 1)
+      |> Enum.reduce(last_txi, fn {micro_block, mbi}, txi ->
+        {txn_mutations, txi} = micro_block_mutations(micro_block, mbi, txi)
+
+        Database.commit(txn_mutations)
+        Producer.commit_enqueued()
+
+        Broadcaster.broadcast_micro_block(micro_block, :mdw)
+        Broadcaster.broadcast_txs(micro_block, :mdw)
+
+        txi
+      end)
+
+    next_kb_model = Model.block(index: {height + 1, -1}, tx_index: next_txi, hash: next_kb_hash)
+
+    block_rewards_mutation =
+      if height >= AE.min_block_reward_height() do
+        IntTransfer.block_rewards_mutation(height, kb_header, kb_hash)
+      end
+
+    Database.commit([
+      block_rewards_mutation,
+      NamesExpirationMutation.new(height),
+      OraclesExpirationMutation.new(height),
+      StatsMutation.new(height, last_mbi == -1),
+      WriteTxnMutation.new(Model.Block, next_kb_model)
+    ])
+
+    Broadcaster.broadcast_key_block(key_block, :mdw)
+  end
+
+  defp micro_block_mutations(mblock, mbi, txi) do
+    height = :aec_blocks.height(mblock)
+    mb_time = :aec_blocks.time_in_msecs(mblock)
+    {:ok, mb_hash} = :aec_headers.hash_header(:aec_blocks.to_micro_header(mblock))
+    mb_txs = :aec_blocks.txs(mblock)
+    events = AeMdw.Contract.get_grouped_events(mblock)
+    tx_ctx = {{height, mbi}, mb_hash, mb_time, events}
+
+    txn_txs_mutations =
+      mb_txs
+      |> Enum.with_index(txi)
+      |> Enum.reduce([], fn {signed_tx, txi}, txn_mutations_acc ->
+        txn_mutations = Transaction.transaction_mutations(signed_tx, txi, tx_ctx)
+
+        txn_mutations_acc ++ txn_mutations
+      end)
+
+    mb_model = Model.block(index: {height, mbi}, tx_index: txi, hash: mb_hash)
+
+    txn_mutations = [WriteTxnMutation.new(Model.Block, mb_model) | txn_txs_mutations]
+
+    {txn_mutations, txi + length(mb_txs)}
+  end
+
+  @spec synced_height :: Blocks.height() | -1
+  def synced_height do
+    case Database.last_key(Model.DeltaStat) do
+      :none -> -1
+      {:ok, height} -> height
+    end
+  end
+end

lib/ae_mdw/db/sync/block_index.ex
@@ -1,68 +1,0 @@
-defmodule AeMdw.Db.Sync.BlockIndex do
-  # credo:disable-for-this-file
-  @moduledoc "fills :block table from backwards to get {height, nil} -> keyblock_hash"
-
-  require AeMdw.Db.Model
-
-  alias AeMdw.Db.Sync
-  alias AeMdw.Db.Model
-  alias AeMdw.Database
-  alias AeMdw.Db.WriteTxnMutation
-  alias AeMdw.Log
-
-  import AeMdw.Util
-
-  @log_freq 10000
-
-  ################################################################################
-
-  def sync(max_height \\ :safe) do
-    max_height = Sync.height(max_height)
-    min_height = (max_kbi() || -1) + 1
-
-    if max_height >= min_height do
-      header = :aec_chain.get_key_header_by_height(max_height) |> ok!
-      initial_hash = :aec_headers.hash_header(header) |> ok!
-
-      {m_block_list, _} =
-        Enum.reduce(max_height..min_height, {[], initial_hash}, fn height, {m_block_list, hash} ->
-          if rem(height, @log_freq) == 0, do: Log.info("syncing block index at #{height}")
-
-          {m_key_block, prev_hash} = sync_key_header(height, hash)
-
-          {[m_key_block | m_block_list], prev_hash}
-        end)
-
-      m_block_list
-      |> Enum.map(&WriteTxnMutation.new(Model.Block, &1))
-      |> Database.commit()
-    end
-
-    max_kbi()
-  end
-
-  def min_kbi() do
-    case Database.first_key(Model.Block) do
-      :none -> nil
-      {:ok, {kbi, _mbi}} -> kbi
-    end
-  end
-
-  def max_kbi() do
-    case Database.last_key(Model.Block) do
-      :none -> nil
-      {:ok, {kbi, _mbi}} -> kbi
-    end
-  end
-
-  ################################################################################
-
-  defp sync_key_header(height, hash) do
-    {:ok, kh} = :aec_chain.get_header(hash)
-    ^height = :aec_headers.height(kh)
-    :key = :aec_headers.type(kh)
-    kb_model = Model.block(index: {height, -1}, hash: hash)
-
-    {kb_model, :aec_headers.prev_key_hash(kh)}
-  end
-end

lib/ae_mdw/db/sync/fork_detector.ex
@@ -1,51 +1,0 @@
-defmodule AeMdw.Db.Sync.ForkDetector do
-  @moduledoc "detects only key forks at the moment"
-
-  use GenServer
-
-  import AeMdw.Util
-
-  ################################################################################
-
-  def start_link(_),
-    do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
-
-  def init([]) do
-    :aec_events.subscribe(:top_changed)
-    {:ok, nil}
-  end
-
-  def handle_info({_, :top_changed, %{info: %{block_type: :key, height: h}}}, s) do
-    case headers(h, :key) do
-      [_, _ | _] = candidates ->
-        {main_header, _} = main_header(candidates)
-        :aec_events.publish(:chain, {:fork, main_header})
-
-      [header] ->
-        :aec_events.publish(:chain, {:generation, header})
-    end
-
-    {:noreply, s}
-  end
-
-  def handle_info({_, :top_changed, %{info: %{block_type: :micro}}}, s),
-    do: {:noreply, s}
-
-  def headers(h, type) when is_integer(h),
-    do: :aec_db.find_headers_at_height(h) |> Enum.filter(&(:aec_headers.type(&1) == type))
-
-  def main_header(candidates) do
-    candidates
-    |> Stream.map(&{&1, ok!(:aec_headers.hash_header(&1))})
-    |> Enum.reduce_while(
-      nil,
-      fn {header, hash}, nil ->
-        # COSTLY!
-        case :aec_chain_state.hash_is_in_main_chain(hash) do
-          true -> {:halt, {header, hash}}
-          false -> {:cont, nil}
-        end
-      end
-    )
-  end
-end

commit a07cbbd3fd851bdf8d5db2a164174a16ae2e4e39

fix: restart sync server after sync fails
bug introduced in #589

lib/ae_mdw/db/status.ex
@@ -5,6 +5,7 @@ defmodule AeMdw.Db.Status do
   alias AeMdw.Db.Model
   alias AeMdw.Db.Util
   alias AeMdw.Sync.AsyncTasks.Stats
+  alias AeMdw.Sync.Server
 
   require Model
 
@@ -14,7 +15,7 @@ defmodule AeMdw.Db.Status do
     {node_syncing?, node_progress} = :aec_sync.sync_progress()
     node_height = :aec_blocks.height(top_kb)
     {mdw_tx_index, mdw_height} = safe_mdw_tx_index_and_height()
-    mdw_syncing? = is_pid(Process.whereis(AeMdw.Sync.Server))
+    mdw_syncing? = Server.syncing?()
     {:ok, version} = :application.get_key(:ae_mdw, :vsn)
     async_tasks_counters = Stats.counters()
 

lib/ae_mdw/sync/server.ex
@@ -15,10 +15,12 @@ defmodule AeMdw.Sync.Server do
 
   use GenServer
 
-  defstruct [:sync_pid, :fork_height, :current_height]
+  defstruct [:sync_pid_ref, :fork_height, :current_height, :restarts, :syncing?]
 
   @unsynced_gens 1
   @max_blocks_sync 600
+  @max_restarts 5
+  @retry_time 3_600_000
 
   @spec start_link(GenServer.options()) :: GenServer.on_start()
   def start_link(_opts),
@@ -27,11 +29,19 @@ defmodule AeMdw.Sync.Server do
   @spec start_sync() :: :ok
   def start_sync, do: GenServer.cast(__MODULE__, :start_sync)
 
+  @spec syncing?() :: boolean()
+  def syncing?, do: GenServer.call(__MODULE__, :syncing?)
+
   @impl true
   def init([]) do
     current_height = Block.synced_height()
 
-    {:ok, %__MODULE__{current_height: current_height}}
+    {:ok, %__MODULE__{current_height: current_height, restarts: 0, syncing?: false}}
+  end
+
+  @impl true
+  def handle_call(:syncing?, _from, %__MODULE__{syncing?: syncing?} = state) do
+    {:reply, syncing?, state}
   end
 
   @impl true
@@ -39,11 +49,11 @@ defmodule AeMdw.Sync.Server do
     :aec_events.subscribe(:chain)
     :aec_events.subscribe(:top_changed)
 
-    {:noreply, process_state(state)}
+    {:noreply, process_state(%__MODULE__{state | syncing?: true, restarts: 0})}
   end
 
   def handle_cast({:done, height}, state) do
-    new_state = process_state(%__MODULE__{state | current_height: height, sync_pid: nil})
+    new_state = process_state(%__MODULE__{state | current_height: height, sync_pid_ref: nil})
 
     {:noreply, new_state}
   end
@@ -85,8 +95,42 @@ defmodule AeMdw.Sync.Server do
   def handle_info({:gproc_ps_event, :top_changed, %{info: %{block_type: :micro}}}, s),
     do: {:noreply, s}
 
+  def handle_info(
+        {:DOWN, ref, :process, pid, reason},
+        %__MODULE__{sync_pid_ref: {pid, ref}, restarts: restarts} = s
+      )
+      when restarts < @max_restarts do
+    Log.info("Sync.Server error: #{inspect(reason)}")
+
+    current_height = Block.synced_height()
+
+    {:noreply,
+     process_state(%__MODULE__{
+       s
+       | restarts: restarts + 1,
+         sync_pid_ref: nil,
+         current_height: current_height
+     })}
+  end
+
+  def handle_info({:DOWN, ref, :process, pid, reason}, %__MODULE__{sync_pid_ref: {pid, ref}} = s) do
+    Log.info("Sync.Server error: #{inspect(reason)}. Stopping..")
+
+    :aec_events.unsubscribe(:chain)
+    :aec_events.unsubscribe(:top_changed)
+
+    :timer.apply_after(@retry_time, __MODULE__, :start_sync, [])
+
+    {:noreply, process_state(%__MODULE__{s | syncing?: false, sync_pid_ref: nil})}
+  end
+
   defp process_state(
-         %__MODULE__{current_height: current_height, sync_pid: nil, fork_height: fork_height} = s
+         %__MODULE__{
+           current_height: current_height,
+           sync_pid_ref: nil,
+           fork_height: fork_height,
+           syncing?: true
+         } = s
        ) do
     top_height = height() - @unsynced_gens
 
@@ -102,13 +146,15 @@ defmodule AeMdw.Sync.Server do
           nil
       end
 
-    %__MODULE__{s | sync_pid: sync_pid, fork_height: nil}
+    sync_pid_ref = {sync_pid, Process.monitor(sync_pid)}
+
+    %__MODULE__{s | sync_pid_ref: sync_pid_ref, fork_height: nil}
   end
 
   defp process_state(s), do: s
 
   defp spawn_sync(from_height, to_height) do
-    spawn_link(fn ->
+    spawn(fn ->
       Block.sync(from_height, to_height)
 
       GenServer.cast(__MODULE__, {:done, to_height})
@@ -116,7 +162,7 @@ defmodule AeMdw.Sync.Server do
   end
 
   defp spawn_invalidate(height) do
-    spawn_link(fn ->
+    spawn(fn ->
       Log.info("invalidation #{height}")
       Invalidate.invalidate(height)
 