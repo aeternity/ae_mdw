dd0a02df52449e161042c2297f7b183a9e4d4551
Rogerio Pontual <rppontual@gmail.com>
2022-06-09 17:06:04 +0100

    feat: set low gas limit according to Node base gas

--- lib/ae_mdw/dry_run/runner.ex
+++ lib/ae_mdw/dry_run/runner.ex
@@ -5,15 +5,16 @@ defmodule AeMdw.DryRun.Runner do
 
   alias AeMdw.DryRun.Contract
 
-  @typep pubkey() :: <<_::256>>
-  @typep block_hash() :: <<_::256>>
+  @typep pubkey() :: AeMdw.Node.Db.pubkey()
+  @typep block_hash() :: AeMdw.Blocks.block_hash()
+  @typep height() :: AeMdw.Blocks.height()
 
   # arbitrary new pk to run the calls
   @runner_pk <<13, 24, 60, 171, 170, 28, 99, 114, 174, 14, 112, 19, 49, 53, 233, 194, 46, 149,
                172, 14, 114, 22, 38, 51, 153, 136, 58, 149, 27, 56, 30, 105>>
 
   @amount trunc(:math.pow(10, 35))
-  @extension_gas_limit 100_000
+  @low_gas_limit_factor 1.5
   @extension_methods ["aex9_extensions", "aex141_extensions"]
 
   @doc """
@@ -29,20 +30,28 @@ defmodule AeMdw.DryRun.Runner do
   @doc """
   Creates a contract call transaction record (without running it).
   """
-  @spec new_contract_call_tx(pubkey(), block_hash(), String.t(), list()) :: tuple()
-  def new_contract_call_tx(contract_pk, block_hash, function_name, args)
-      when function_name in @extension_methods do
+  @spec new_contract_call_tx(
+          pubkey(),
+          height(),
+          block_hash(),
+          AeMdw.Contract.method_name(),
+          AeMdw.Contract.method_args()
+        ) :: tuple()
+  def new_contract_call_tx(contract_pk, height, block_hash, function_name, args)
+      when function_name == "meta_info" or function_name in @extension_methods do
+    base_gas = Contract.call_tx_base_gas(height)
+
     Contract.new_call_tx(
       @runner_pk,
       contract_pk,
       block_hash,
       function_name,
       args,
-      @extension_gas_limit
+      trunc(base_gas * @low_gas_limit_factor)
     )
   end
 
-  def new_contract_call_tx(contract_pk, block_hash, function_name, args) do
+  def new_contract_call_tx(contract_pk, _height, block_hash, function_name, args) do
     Contract.new_call_tx(@runner_pk, contract_pk, block_hash, function_name, args)
   end
 end
