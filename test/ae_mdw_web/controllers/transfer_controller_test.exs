25567be8c83cd815ce1124c811edef34a5e47e6a
Rogerio Pontual <rppontual@gmail.com>
2022-07-26 17:41:04 +0100

    test: add account param test and validates all fields

--- test/ae_mdw_web/controllers/transfer_controller_test.exs
+++ test/ae_mdw_web/controllers/transfer_controller_test.exs
@@ -44,8 +44,17 @@ defmodule AeMdwWeb.TransferControllerTest do
 
       assert Enum.count(response["data"]) == 100
 
-      assert Enum.all?(response["data"], fn %{"kind" => kind} ->
-               kind == kind_prefix
+      fortuna_height =
+        :aec_governance.get_network_id()
+        |> :aec_hard_forks.protocols_from_network_id()
+        |> Map.get(3)
+
+      assert Enum.all?(response["data"], fn %{
+                                              "kind" => kind,
+                                              "amount" => amount,
+                                              "height" => height
+                                            } ->
+               kind == kind_prefix and amount > 0 and height == fortuna_height
              end)
 
       conn_next = get(conn, response["next"])
@@ -53,9 +62,44 @@ defmodule AeMdwWeb.TransferControllerTest do
 
       assert Enum.count(response_next["data"]) == 100
 
-      assert Enum.all?(response_next["data"], fn %{"kind" => kind} ->
-               kind == kind_prefix
+      assert Enum.all?(response_next["data"], fn %{
+                                                   "account_id" => account_id,
+                                                   "kind" => kind,
+                                                   "amount" => amount,
+                                                   "height" => height
+                                                 } ->
+               if account_id in [
+                    "ak_292g7TxJNGFfFXxsvFhJ5MfP6RzMrFGMJxRaY4Tx7byaBFQTg4",
+                    "ak_2qdNcDDcELtsSPwot9ojjojuUxsucpCTcqbqZPT4nKjo1wtEEj"
+                  ] do
+                 kind == kind_prefix and amount == 0 and height == fortuna_height
+               else
+                 kind == kind_prefix and amount > 0 and height == fortuna_height
+               end
              end)
+    end
+
+    test "returns a lima account transfer filtering by account", %{
+      conn: conn
+    } do
+      account_id = "ak_zGR3f3QQ3BDAwhuPktLanrqcb6vrbViBr8RzU5VeqGNDbEyx9"
+
+      conn = get(conn, "/v2/transfers", direction: "forward", account: account_id)
+      response = json_response(conn, 200)
+
+      lima_height =
+        :aec_governance.get_network_id()
+        |> :aec_hard_forks.protocols_from_network_id()
+        |> Map.get(4)
+
+      assert Enum.count(response["data"]) == 1
+
+      assert %{
+               "account_id" => ^account_id,
+               "kind" => "accounts_lima",
+               "amount" => 1_186_111_575_000_000_000_000,
+               "height" => ^lima_height
+             } = List.first(response["data"])
     end
 
     test "renders error when the range is invalid", %{conn: conn} do
