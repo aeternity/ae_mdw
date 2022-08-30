8e2be75e5c7d922ff5aaf3460162b86670f14b87
Rogerio Pontual <44991200+jyeshe@users.noreply.github.com>
2022-08-30 14:49:22 +0100

    fix: convert transfer event token_id to integer (#878)

--- test/ae_mdw/db/contract_call_mutation_test.exs
+++ test/ae_mdw/db/contract_call_mutation_test.exs
@@ -10,6 +10,7 @@ defmodule AeMdw.Db.ContractCallMutationTest do
   alias AeMdw.Validate
 
   import AeMdw.Node.ContractCallFixtures
+  import AeMdwWeb.Helpers.AexnHelper, only: [enc_id: 1]
 
   import Mock
   require Model
@@ -205,6 +206,65 @@ defmodule AeMdw.Db.ContractCallMutationTest do
         assert State.exists?(state, Model.IdxAex9Transfer, {call_txi, 0, from_pk, to_pk, amount})
         assert State.exists?(state, Model.Aex9PairTransfer, {from_pk, to_pk, call_txi, amount, 0})
       end
+    end
+  end
+
+  describe "aex141 transfer" do
+    test "add aex141 transfers after a call with transfer logs" do
+      contract_pk = :crypto.strong_rand_bytes(32)
+      from_pk = :crypto.strong_rand_bytes(32)
+      to_pk = :crypto.strong_rand_bytes(32)
+      height = Enum.random(100_000..999_999)
+      call_txi = Enum.random(10_000_000..99_999_999)
+      token_id = Enum.random(1..100_000)
+
+      fun_arg_res = %{
+        arguments: [
+          %{
+            type: :address,
+            value: enc_id(from_pk)
+          },
+          %{
+            type: :address,
+            value: enc_id(to_pk)
+          },
+          %{type: :int, value: token_id}
+        ],
+        function: "transfer",
+        result: %{type: :unit, value: ""}
+      }
+
+      call_rec =
+        {:call, :crypto.strong_rand_bytes(32), {:id, :account, :crypto.strong_rand_bytes(32)}, 1,
+         height, {:id, :contract, contract_pk}, 1_000_000_000, 42_000, "?", :ok,
+         [
+           {contract_pk,
+            [AeMdw.Node.aexn_transfer_event_hash(), from_pk, to_pk, <<token_id::256>>], ""}
+         ]}
+
+      mutation =
+        ContractCallMutation.new(
+          contract_pk,
+          {height, 0},
+          call_txi,
+          fun_arg_res,
+          call_rec
+        )
+
+      state =
+        NullStore.new()
+        |> MemStore.new()
+        |> State.new()
+        |> State.put(Model.AexnContract, Model.aexn_contract(index: {:aex141, contract_pk}))
+        |> State.put(
+          Model.AexnContract,
+          Model.nft_ownership(index: {from_pk, contract_pk, token_id})
+        )
+        |> State.cache_put(:ct_create_sync_cache, contract_pk, call_txi - 1)
+        |> State.commit_mem([mutation])
+
+      assert State.exists?(state, Model.NftOwnership, {to_pk, contract_pk, token_id})
+      refute State.exists?(state, Model.NftOwnership, {from_pk, contract_pk, token_id})
     end
   end
 
