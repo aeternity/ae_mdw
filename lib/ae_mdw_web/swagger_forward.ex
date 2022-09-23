1912a9b6902f853743caef5d1520ab217f7dcbf8
Rogerio Pontual <rppontual@gmail.com>
2022-09-23 11:22:11 +0900

    docs: add type specs for forward functions

--- lib/ae_mdw_web/swagger_forward.ex
+++ lib/ae_mdw_web/swagger_forward.ex
@@ -4,10 +4,14 @@ defmodule AeMdwWeb.SwaggerForward do
   """
   use AeMdwWeb, :controller
 
+  alias Plug.Conn
+
+  @spec index_v1(Conn.t(), any()) :: Conn.t()
   def index_v1(conn, _params) do
     redirect(conn, to: "/swagger/index_v1.html")
   end
 
+  @spec index_v2(Conn.t(), any()) :: Conn.t()
   def index_v2(conn, _params) do
     redirect(conn, to: "/swagger/index_v2.html")
   end
