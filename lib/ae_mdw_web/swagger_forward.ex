c810a371c8d6d0193a625e3dab0521a475c4f944
Rogerio Pontual <rppontual@gmail.com>
2022-09-23 18:23:00 +0900

    chore: unify index html for v1 and v2

--- lib/ae_mdw_web/swagger_forward.ex
+++ lib/ae_mdw_web/swagger_forward.ex
@@ -7,12 +7,17 @@ defmodule AeMdwWeb.SwaggerForward do
   alias Plug.Conn
 
   @spec index_v1(Conn.t(), any()) :: Conn.t()
-  def index_v1(conn, _params) do
-    redirect(conn, to: "/swagger/index_v1.html")
+  def index_v1(%Plug.Conn{path_info: [begin | _path]} = conn, _params) do
+    redirect(conn, to: swagger_path(begin, "v1"))
   end
 
   @spec index_v2(Conn.t(), any()) :: Conn.t()
-  def index_v2(conn, _params) do
-    redirect(conn, to: "/swagger/index_v2.html")
+  def index_v2(%Plug.Conn{path_info: [begin | _path]} = conn, _params) do
+    redirect(conn, to: swagger_path(begin, "v2"))
+  end
+
+  defp swagger_path(begin, version) do
+    prefix = if not String.contains?(begin, "mdw"), do: "/mdw"
+    "#{prefix}/swagger/index.html?version=#{version}"
   end
 end
