8529401fa3bf23110f9fda7a730c72f52f706b7e
Sebastian Borrazas <seba.borrazas@gmail.com>
2023-03-06 07:48:39 -0300

    docs: use /api and /v2/api path for swagger spec files to match node
    refs #108
    refs #674

--- lib/ae_mdw_web/controllers/util_controller.ex
+++ lib/ae_mdw_web/controllers/util_controller.ex
@@ -8,11 +8,18 @@ defmodule AeMdwWeb.UtilController do
   alias AeMdw.Error.Input
   alias Plug.Conn
 
-  @spec status(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  @spec status(Conn.t(), map()) :: Conn.t()
   def status(%Conn{assigns: %{state: state}} = conn, _params),
     do: json(conn, Status.node_and_mdw_status(state))
 
-  @spec no_route(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  @spec no_route(Conn.t(), map()) :: Conn.t()
   def no_route(conn, _params),
     do: AeMdwWeb.Util.send_error(conn, Input.NotFound, "no such route")
+
+  @spec static_file(Conn.t(), map()) :: Conn.t()
+  def static_file(%Conn{assigns: %{filepath: filepath}} = conn, _params) do
+    filepath = Application.app_dir(:ae_mdw, Path.join("priv", filepath))
+
+    send_file(conn, 200, filepath)
+  end
 end
