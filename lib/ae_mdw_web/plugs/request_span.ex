7a6474fca6b28beabf78e43a147a6e1012357e47
Rogerio Pontual <rppontual@gmail.com>
2023-03-09 14:29:11 +0000

    refactor: use pattern matching style

--- lib/ae_mdw_web/plugs/request_span.ex
+++ lib/ae_mdw_web/plugs/request_span.ex
@@ -18,11 +18,18 @@ defmodule AeMdwWeb.Plugs.RequestSpan do
   end
 
   @spec emit_event_callback(Conn.t()) :: Conn.t()
-  def emit_event_callback(conn) do
-    duration = System.monotonic_time(:millisecond) - conn.assigns.start_time
+  def emit_event_callback(
+        %Conn{
+          assigns: %{start_time: start_time},
+          method: method,
+          request_path: request_path,
+          host: host
+        } = conn
+      ) do
+    duration = System.monotonic_time(:millisecond) - start_time
 
     with %{route: route} <-
-           Phoenix.Router.route_info(AeMdwWeb.Router, conn.method, conn.request_path, conn.host),
+           Phoenix.Router.route_info(AeMdwWeb.Router, method, request_path, host),
          [request_id | _ignored] <- Conn.get_resp_header(conn, "x-request-id") do
       metadata = %{route: route, request_id: request_id}
       :telemetry.execute([:ae_mdw, :http, :request], %{duration: duration}, metadata)
