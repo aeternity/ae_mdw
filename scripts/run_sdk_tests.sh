50e4e9a35e79b3f5db350b49d328fc07ac4b84b6
Sebastian Borrazas <seba.borrazas@gmail.com>
2023-05-10 12:46:13 -0300

    fix: adjust test-devmode script

--- scripts/run_sdk_tests.sh
+++ scripts/run_sdk_tests.sh
@@ -12,8 +12,8 @@ sleep 10
 
 docker-compose -f docker-compose-dev.yml exec ae_mdw ./bin/ae_mdw rpc ':aeplugin_dev_mode_app.start_unlink()'
 
-docker-compose -f docker-compose-dev.yml run node_sdk node index.js
+docker-compose -f docker-compose-dev.yml run node_sdk /bin/bash -c "yarn install && node index.js"
 
 docker-compose -f docker-compose-dev.yml stop ae_mdw
 
-docker-compose -f docker-compose-dev.yml run --workdir=/app ae_mdw elixir --sname aeternity@localhost -S mix test.devmode
+docker-compose -f docker-compose-dev.yml run --workdir=/app ae_mdw scripts/test-devmode.sh
