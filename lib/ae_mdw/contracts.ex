d39bcd117aa5ca273a0a3828983e57f00f8b703f
Rogerio Pontual <rppontual@gmail.com>
2023-06-02 15:45:33 +0200

    chore: add function parameter alias to contract logs

--- lib/ae_mdw/contracts.ex
+++ lib/ae_mdw/contracts.ex
@@ -517,6 +517,10 @@ defmodule AeMdw.Contracts do
     do: {:event_hash, :aec_hash.blake2b_256_hash(ctor_name)}
 
   defp convert_param(_state, {"function", fname}) when byte_size(fname) > 0, do: {:fname, fname}
+
+  defp convert_param(state, {"function_prefix", fname}),
+    do: convert_param(state, {"function", fname})
+
   defp convert_param(_state, {"aexn-args", _}), do: {:ignore, nil}
 
   defp convert_param(_state, {id_key, id_val}) do
