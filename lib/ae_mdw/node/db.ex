de3db7cb7bce408b5ed1cf0797dcbc2390613f1d
Sebastian Borrazas <seba.borrazas@gmail.com>
2023-06-27 07:43:37 -0300

    feat: add micro block time to oracle nested resources (#1419)

--- lib/ae_mdw/node/db.ex
+++ lib/ae_mdw/node/db.ex
@@ -258,6 +258,10 @@ defmodule AeMdw.Node.Db do
     end
   end
 
+  @spec get_block_time(Blocks.block_hash()) :: key_block() | micro_block()
+  def get_block_time(block_hash),
+    do: block_hash |> :aec_db.get_block() |> :aec_blocks.time_in_msecs()
+
   defp block_accounts_tree(mb_hash) do
     {:value, micro_block} = :aec_db.find_block(mb_hash)
     header = :aec_blocks.to_header(micro_block)
