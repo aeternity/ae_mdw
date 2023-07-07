d2ba17aa6742636845482cc9c91259ffa9a0cf24
Rogerio Pontual <44991200+jyeshe@users.noreply.github.com>
2023-07-07 14:37:00 +0100

    test: leave controller tests to coverage (#1451)

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,5 @@
 set -xe
 
 mix local.hex --force && mix local.rebar --force && mix deps.get
-elixir --sname aeternity@localhost -S mix test
-
+elixir --sname aeternity@localhost -S mix test test/ae_mdw
 exit 0
