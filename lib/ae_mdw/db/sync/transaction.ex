1b5f77efbf34c8efb79c7f9f1b370c0876809cd5
Sebastian Borrazas <seba.borrazas@gmail.com>
2023-07-10 07:22:09 -0300

    chore: compact type increment counts mutations into a single one (#1453)

--- lib/ae_mdw/db/sync/transaction.ex
+++ lib/ae_mdw/db/sync/transaction.ex
@@ -10,7 +10,6 @@ defmodule AeMdw.Db.Sync.Transaction do
   alias AeMdw.Db.ContractCallMutation
   alias AeMdw.Db.ContractCreateMutation
   alias AeMdw.Db.ContractCreateCacheMutation
-  alias AeMdw.Db.IncrementTypeCountMutation
   alias AeMdw.Db.Model
   alias AeMdw.Db.NameRevokeMutation
   alias AeMdw.Db.NameTransferMutation
@@ -99,7 +98,6 @@ defmodule AeMdw.Db.Sync.Transaction do
     [
       WriteMutation.new(Model.Tx, m_tx),
       WriteMutation.new(Model.Type, Model.type(index: {type, txi})),
-      IncrementTypeCountMutation.new(type),
       WriteMutation.new(Model.Time, Model.time(index: {mb_time, txi})),
       WriteFieldsMutation.new(type, tx, block_index, txi)
       | tx_mutations(tx_context)
