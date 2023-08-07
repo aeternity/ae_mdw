29e087fa85b281631c74cce79c62d2cc5046c10c
Sebastian Borrazas <seba.borrazas@gmail.com>
2023-08-07 13:11:48 -0300

    test: restructure stats_test as per demand

--- test/ae_mdw/db/sync/stats_test.exs
+++ test/ae_mdw/db/sync/stats_test.exs
@@ -1,7 +1,6 @@
 defmodule AeMdw.Db.Sync.StatsTest do
   use ExUnit.Case
 
-  alias AeMdw.Db.Model
   alias AeMdw.Db.Sync.Stats
   alias AeMdw.Db.StatisticsMutation
 
@@ -12,27 +11,42 @@ defmodule AeMdw.Db.Sync.StatsTest do
     end
 
     test "when count > 0, it returnsa mutation with all stats changes" do
-      time = 1_691_422_166_000
+      time1 = 0
+      time2 = 31_536_000_000
 
       type_counts = %{
         spend_tx: 1,
         oracle_register_tx: 18
       }
 
-      mutation =
+      mutation1 =
         StatisticsMutation.new([
-          {{{:transactions, :all}, :day, 19_576}, 19},
-          {{{:transactions, :oracle_register_tx}, :day, 19_576}, 18},
-          {{{:transactions, :spend_tx}, :day, 19_576}, 1},
-          {{{:transactions, :all}, :week, 2_796}, 19},
-          {{{:transactions, :oracle_register_tx}, :week, 2_796}, 18},
-          {{{:transactions, :spend_tx}, :week, 2_796}, 1},
-          {{{:transactions, :all}, :month, 644}, 19},
-          {{{:transactions, :oracle_register_tx}, :month, 644}, 18},
-          {{{:transactions, :spend_tx}, :month, 644}, 1}
+          {{{:transactions, :all}, :day, 0}, 19},
+          {{{:transactions, :oracle_register_tx}, :day, 0}, 18},
+          {{{:transactions, :spend_tx}, :day, 0}, 1},
+          {{{:transactions, :all}, :week, 0}, 19},
+          {{{:transactions, :oracle_register_tx}, :week, 0}, 18},
+          {{{:transactions, :spend_tx}, :week, 0}, 1},
+          {{{:transactions, :all}, :month, 1}, 19},
+          {{{:transactions, :oracle_register_tx}, :month, 1}, 18},
+          {{{:transactions, :spend_tx}, :month, 1}, 1}
         ])
 
-      assert ^mutation = Stats.txs_statistics_mutations(time, type_counts)
+      mutation2 =
+        StatisticsMutation.new([
+          {{{:transactions, :all}, :day, 365}, 19},
+          {{{:transactions, :oracle_register_tx}, :day, 365}, 18},
+          {{{:transactions, :spend_tx}, :day, 365}, 1},
+          {{{:transactions, :all}, :week, 52}, 19},
+          {{{:transactions, :oracle_register_tx}, :week, 52}, 18},
+          {{{:transactions, :spend_tx}, :week, 52}, 1},
+          {{{:transactions, :all}, :month, 13}, 19},
+          {{{:transactions, :oracle_register_tx}, :month, 13}, 18},
+          {{{:transactions, :spend_tx}, :month, 13}, 1}
+        ])
+
+      assert ^mutation1 = Stats.txs_statistics_mutations(time1, type_counts)
+      assert ^mutation2 = Stats.txs_statistics_mutations(time2, type_counts)
     end
   end
 end
