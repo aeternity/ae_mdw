1a01b5e7ee7ccb12521abd6b02cde387b005d603
Rogerio Pontual <44991200+jyeshe@users.noreply.github.com>
2023-08-11 12:58:26 +0200

    refactor: remove name cache (#1519)

--- lib/ae_mdw/db/mutations/name_claim_mutation.ex
+++ lib/ae_mdw/db/mutations/name_claim_mutation.ex
@@ -86,14 +86,13 @@ defmodule AeMdw.Db.NameClaimMutation do
         },
         state
       ) do
-    m_owner = Model.owner(index: {owner_pk, plain_name})
     m_plain_name = Model.plain_name(index: name_hash, value: plain_name)
 
-    state2 = Name.cache_through_write(state, Model.PlainName, m_plain_name)
+    state2 = State.put(state, Model.PlainName, m_plain_name)
 
     case timeout do
       0 ->
-        previous = Util.ok_nil(Name.cache_through_read(state, Model.InactiveName, plain_name))
+        previous = Util.ok_nil(State.get(state, Model.InactiveName, plain_name))
         expire = Names.expire_after(height)
 
         m_name =
@@ -106,26 +105,16 @@ defmodule AeMdw.Db.NameClaimMutation do
             auction_timeout: 0
           )
 
-        m_name_activation = Model.activation(index: {height, plain_name})
-        m_name_exp = Model.expiration(index: {expire, plain_name})
         lock_amount = (lima_or_higher? && name_fee) || :aec_governance.name_claim_locked_fee()
-
-        m_name_owner_deactivation =
-          Model.owner_deactivation(index: {owner_pk, expire, plain_name})
 
         name_claim = Model.name_claim(index: {plain_name, height, txi_idx})
 
         state2
-        |> Name.cache_through_write(Model.ActiveName, m_name)
-        |> Name.cache_through_write(Model.ActiveNameOwner, m_owner)
-        |> Name.cache_through_write(Model.ActiveNameActivation, m_name_activation)
-        |> Name.cache_through_write(Model.ActiveNameExpiration, m_name_exp)
-        |> State.put(Model.ActiveNameOwnerDeactivation, m_name_owner_deactivation)
-        |> Name.cache_through_delete_inactive(previous)
-        |> IntTransfer.fee({height, txi_idx}, :lock_name, owner_pk, txi_idx, lock_amount)
-        |> State.inc_stat(:names_activated)
-        |> State.inc_stat(:burned_in_auctions, lock_amount)
+        |> Name.put_active(m_name)
         |> State.put(Model.NameClaim, name_claim)
+        |> Name.delete_inactive(previous)
+        |> IntTransfer.fee({height, txi_idx}, :lock_name, owner_pk, txi_idx, lock_amount)
+        |> State.inc_stat(:burned_in_auctions, lock_amount)
 
       timeout ->
         auction_end = height + timeout
@@ -145,8 +134,8 @@ defmodule AeMdw.Db.NameClaimMutation do
           IntTransfer.fee(state2, {height, txi_idx}, :spend_name, owner_pk, txi_idx, name_fee)
 
         state4 =
-          case Name.cache_through_read(state3, Model.AuctionBid, plain_name) do
-            nil ->
+          case State.get(state3, Model.AuctionBid, plain_name) do
+            :not_found ->
               State.inc_stat(state3, :auctions_started)
 
             {:ok,
@@ -159,9 +148,9 @@ defmodule AeMdw.Db.NameClaimMutation do
               prev_name_fee = :aens_claim_tx.name_fee(prev_name_claim_tx)
 
               state3
-              |> Name.cache_through_delete(Model.AuctionBid, plain_name)
-              |> Name.cache_through_delete(Model.AuctionOwner, {prev_owner, plain_name})
-              |> Name.cache_through_delete(
+              |> State.delete(Model.AuctionBid, plain_name)
+              |> State.delete(Model.AuctionOwner, {prev_owner, plain_name})
+              |> State.delete(
                 Model.AuctionExpiration,
                 {prev_auction_end, plain_name}
               )
@@ -176,9 +165,9 @@ defmodule AeMdw.Db.NameClaimMutation do
           end
 
         state4
-        |> Name.cache_through_write(Model.AuctionBid, m_auction_bid)
-        |> Name.cache_through_write(Model.AuctionOwner, m_owner)
-        |> Name.cache_through_write(Model.AuctionExpiration, m_auction_exp)
+        |> State.put(Model.AuctionBid, m_auction_bid)
+        |> State.put(Model.AuctionOwner, Model.owner(index: {owner_pk, plain_name}))
+        |> State.put(Model.AuctionExpiration, m_auction_exp)
         |> State.put(Model.AuctionBidClaim, auction_claim)
     end
   end
--- lib/ae_mdw/db/sync/name.ex
+++ lib/ae_mdw/db/sync/name.ex
@@ -5,6 +5,7 @@ defmodule AeMdw.Db.Sync.Name do
   alias AeMdw.Collection
   alias AeMdw.Db.Model
   alias AeMdw.Db.Mutation
+  alias AeMdw.Db.Name
   alias AeMdw.Names
   alias AeMdw.Db.IntTransfer
   alias AeMdw.Db.NameClaimMutation
@@ -21,39 +22,8 @@ defmodule AeMdw.Db.Sync.Name do
 
   @type update_type() :: {:update_expiration, Blocks.height()} | :expire | :update
 
-  @typep pubkey :: Db.pubkey()
-  @typep state() :: State.t()
-  @typep table ::
-           Model.ActiveName
-           | Model.InactiveName
-           | Model.ActiveNameActivation
-           | Model.ActiveNameExpiration
-           | Model.InactiveNameExpiration
-           | Model.ActiveNameOwnerDeactivation
-           | Model.InactiveNameOwnerDeactivation
-           | Model.AuctionExpiration
-           | Model.AuctionBid
-           | Model.AuctionOwner
-           | Model.PlainName
-           | Model.ActiveNameOwner
-           | Model.InactiveNameOwner
-           | Model.Pointee
-  @typep name_record() ::
-           Model.name()
-           | Model.activation()
-           | Model.expiration()
-           | Model.plain_name()
-           | Model.owner()
-           | Model.pointee()
-           | Model.auction_bid()
-           | Model.owner_deactivation()
-  @typep cache_key ::
-           String.t()
-           | {Blocks.height(), pubkey()}
-           | {pubkey(), String.t()}
-           | pubkey()
-           | {String.t(), <<>>, <<>>, <<>>, <<>>}
-           | {pubkey(), Blocks.height(), binary()}
+  @typep state :: State.t()
+  @typep deactivate_stat :: :names_expired | :names_revoked
 
   @spec name_claim_mutations(Node.tx(), Txs.tx_hash(), Blocks.block_index(), Txs.txi_idx()) :: [
           Mutation.t()
@@ -121,17 +91,17 @@ defmodule AeMdw.Db.Sync.Name do
           Blocks.block_index()
         ) :: State.t()
   def update(state, name_hash, update_type, pointers, txi_idx, {height, _mbi} = bi) do
-    plain_name = plain_name!(state, name_hash)
+    plain_name = Name.plain_name!(state, name_hash)
 
     Model.name(active: active, expire: old_expire, owner: owner_pk) =
-      m_name = cache_through_read!(state, Model.ActiveName, plain_name)
+      m_name = State.fetch!(state, Model.ActiveName, plain_name)
 
     state2 =
       pointers
       |> Enum.reduce(state, fn ptr, state ->
         m_pointee = Model.pointee(index: pointee_key(ptr, {bi, txi_idx}))
 
-        cache_through_write(state, Model.Pointee, m_pointee)
+        State.put(state, Model.Pointee, m_pointee)
       end)
       |> State.put(Model.NameUpdate, Model.name_update(index: {plain_name, active, txi_idx}))
 
@@ -144,21 +114,18 @@ defmodule AeMdw.Db.Sync.Name do
           Model.owner_deactivation(index: {owner_pk, new_expire, plain_name})
 
         state2
-        |> cache_through_delete(Model.ActiveNameExpiration, {old_expire, plain_name})
-        |> cache_through_delete(
+        |> State.delete(Model.ActiveNameExpiration, {old_expire, plain_name})
+        |> State.delete(
           Model.ActiveNameOwnerDeactivation,
           {owner_pk, old_expire, plain_name}
         )
-        |> cache_through_write(Model.ActiveNameExpiration, new_m_name_exp)
-        |> cache_through_write(Model.ActiveNameOwnerDeactivation, m_name_owner_deactivation)
-        |> cache_through_write(Model.ActiveName, new_m_name)
+        |> State.put(Model.ActiveNameExpiration, new_m_name_exp)
+        |> State.put(Model.ActiveNameOwnerDeactivation, m_name_owner_deactivation)
+        |> State.put(Model.ActiveName, new_m_name)
 
       :expire ->
         new_m_name = Model.name(m_name, expire: height)
-
-        state2
-        |> deactivate_name(height, old_expire, new_m_name)
-        |> State.inc_stat(:names_expired)
+        deactivate_name(state2, height, old_expire, new_m_name, :names_expired)
 
       :update ->
         state
@@ -168,9 +135,9 @@ defmodule AeMdw.Db.Sync.Name do
   @spec transfer(State.t(), Names.name_hash(), Db.pubkey(), Txs.txi_idx(), Blocks.block_index()) ::
           State.t()
   def transfer(state, name_hash, new_owner, txi_idx, block_index) do
-    plain_name = plain_name!(state, name_hash)
+    plain_name = Name.plain_name!(state, name_hash)
 
-    m_name = cache_through_read!(state, Model.ActiveName, plain_name)
+    m_name = State.fetch!(state, Model.ActiveName, plain_name)
     Model.name(active: active, owner: old_owner, expire: expire) = m_name
 
     m_name = Model.name(m_name, active: active, owner: new_owner)
@@ -179,50 +146,43 @@ defmodule AeMdw.Db.Sync.Name do
     name_transfer = Model.name_transfer(index: {plain_name, active, {block_index, txi_idx}})
 
     state
-    |> cache_through_delete(Model.ActiveNameOwner, {old_owner, plain_name})
-    |> cache_through_delete(Model.ActiveNameOwnerDeactivation, {old_owner, expire, plain_name})
-    |> cache_through_write(Model.ActiveNameOwner, m_owner)
-    |> cache_through_write(Model.ActiveName, m_name)
-    |> cache_through_write(Model.ActiveNameOwnerDeactivation, m_name_owner_deactivation)
+    |> State.delete(Model.ActiveNameOwner, {old_owner, plain_name})
+    |> State.delete(Model.ActiveNameOwnerDeactivation, {old_owner, expire, plain_name})
+    |> State.put(Model.ActiveNameOwner, m_owner)
+    |> State.put(Model.ActiveName, m_name)
+    |> State.put(Model.ActiveNameOwnerDeactivation, m_name_owner_deactivation)
     |> State.put(Model.NameTransfer, name_transfer)
   end
 
   @spec revoke(State.t(), Names.name_hash(), Txs.txi_idx(), Blocks.block_index()) :: State.t()
   def revoke(state, name_hash, txi_idx, {height, _mbi} = bi) do
-    plain_name = plain_name!(state, name_hash)
+    plain_name = Name.plain_name!(state, name_hash)
 
-    Model.name(expire: expiration) =
-      m_name = cache_through_read!(state, Model.ActiveName, plain_name)
+    Model.name(expire: expiration) = m_name = State.fetch!(state, Model.ActiveName, plain_name)
 
     m_name = Model.name(m_name, revoke: {bi, txi_idx})
 
-    state
-    |> deactivate_name(height, expiration, m_name)
-    |> State.inc_stat(:names_revoked)
+    deactivate_name(state, height, expiration, m_name, :names_revoked)
   end
 
   @spec expire_name(state(), Blocks.height(), Names.plain_name()) :: state()
   def expire_name(state, height, plain_name) do
-    Model.name(expire: expiration) =
-      m_name = cache_through_read!(state, Model.ActiveName, plain_name)
+    Model.name(expire: expiration) = m_name = State.fetch!(state, Model.ActiveName, plain_name)
 
-    state
-    |> deactivate_name(height, expiration, m_name)
-    |> State.inc_stat(:names_expired)
+    deactivate_name(state, height, expiration, m_name, :names_expired)
   end
 
   @spec expire_auction(state(), Blocks.height(), Names.plain_name()) :: state()
   def expire_auction(state, height, plain_name) do
-    {:ok,
-     Model.auction_bid(
-       block_index_txi_idx: {{last_bid_height, _mbi} = _block_index, txi_idx},
-       owner: owner
-     )} = cache_through_read(state, Model.AuctionBid, plain_name)
+    Model.auction_bid(
+      block_index_txi_idx: {{last_bid_height, _mbi} = _block_index, txi_idx},
+      owner: owner
+    ) = State.fetch!(state, Model.AuctionBid, plain_name)
 
     previous =
-      case cache_through_read(state, Model.InactiveName, plain_name) do
+      case State.get(state, Model.InactiveName, plain_name) do
         {:ok, inactive_name} -> inactive_name
-        _not_found_or_nil -> nil
+        :not_found -> nil
       end
 
     expire = Names.expire_after(height)
@@ -237,11 +197,6 @@ defmodule AeMdw.Db.Sync.Name do
         previous: previous
       )
 
-    m_name_activation = Model.activation(index: {height, plain_name})
-    m_name_exp = Model.expiration(index: {expire, plain_name})
-    m_owner = Model.owner(index: {owner, plain_name})
-    m_name_owner_deactivation = Model.owner_deactivation(index: {owner, expire, plain_name})
-
     name_fee =
       state
       |> DbUtil.read_node_tx(txi_idx)
@@ -255,77 +210,49 @@ defmodule AeMdw.Db.Sync.Name do
       |> State.put(Model.NameClaim, Model.name_claim(index: {plain_name, height, claim_txi_idx}))
       |> State.delete(Model.AuctionBidClaim, {plain_name, height, claim_txi_idx})
     end)
-    |> cache_through_write(Model.ActiveName, m_name)
-    |> cache_through_write(Model.ActiveNameOwner, m_owner)
-    |> cache_through_write(Model.ActiveNameActivation, m_name_activation)
-    |> cache_through_write(Model.ActiveNameExpiration, m_name_exp)
-    |> cache_through_write(Model.ActiveNameOwnerDeactivation, m_name_owner_deactivation)
-    |> cache_through_delete(Model.AuctionExpiration, {height, plain_name})
-    |> cache_through_delete(Model.AuctionOwner, {owner, plain_name})
-    |> cache_through_delete(Model.AuctionBid, plain_name)
-    |> cache_through_delete_inactive(previous)
+    |> put_active(m_name)
+    |> State.delete(Model.AuctionExpiration, {height, plain_name})
+    |> State.delete(Model.AuctionOwner, {owner, plain_name})
+    |> State.delete(Model.AuctionBid, plain_name)
+    |> delete_inactive(previous)
     |> IntTransfer.fee({height, -1}, :lock_name, owner, txi_idx, name_fee)
-    |> State.inc_stat(:names_activated)
     |> State.inc_stat(:auctions_expired)
     |> State.inc_stat(:burned_in_auctions, name_fee)
     |> State.inc_stat(:locked_in_auctions, -name_fee)
   end
 
-  @spec cache_through_write(state(), table(), name_record()) :: state()
-  def cache_through_write(state, table, record) do
+  @spec put_active(state(), Model.name()) :: state()
+  def put_active(
+        state,
+        Model.name(index: plain_name, active: height, expire: expire, owner: owner_pk) = m_name
+      ) do
+    m_owner = Model.owner(index: {owner_pk, plain_name})
+    m_activation = Model.activation(index: {height, plain_name})
+    m_expiration = Model.expiration(index: {expire, plain_name})
+    m_owner_deactivation = Model.owner_deactivation(index: {owner_pk, expire, plain_name})
+
     state
-    |> State.cache_put(:name_sync_cache, {table, elem(record, 1)}, record)
-    |> State.put(table, record)
+    |> State.put(Model.ActiveName, m_name)
+    |> State.put(Model.ActiveNameOwner, m_owner)
+    |> State.put(Model.ActiveNameActivation, m_activation)
+    |> State.put(Model.ActiveNameExpiration, m_expiration)
+    |> State.put(Model.ActiveNameOwnerDeactivation, m_owner_deactivation)
+    |> State.inc_stat(:names_activated)
   end
 
-  @spec cache_through_delete(state(), table(), cache_key()) :: state()
-  def cache_through_delete(state, table, key) do
-    state
-    |> State.cache_delete(:name_sync_cache, {table, key})
-    |> State.delete(table, key)
-  end
-
-  defp cache_through_delete_active(
-         state,
-         expiration,
-         Model.name(index: plain_name, active: active_from, owner: owner_pk)
-       ) do
-    state
-    |> cache_through_delete(Model.ActiveName, plain_name)
-    |> cache_through_delete(Model.ActiveNameOwner, {owner_pk, plain_name})
-    |> cache_through_delete(Model.ActiveNameActivation, {active_from, plain_name})
-    |> cache_through_delete(Model.ActiveNameExpiration, {expiration, plain_name})
-    |> cache_through_delete(Model.ActiveNameOwnerDeactivation, {owner_pk, expiration, plain_name})
-  end
-
-  @spec cache_through_read(state(), table(), cache_key()) :: {:ok, name_record()} | nil
-  def cache_through_read(state, table, key) do
-    case State.cache_get(state, :name_sync_cache, {table, key}) do
-      {:ok, record} ->
-        {:ok, record}
-
-      :not_found ->
-        case State.get(state, table, key) do
-          {:ok, record} -> {:ok, record}
-          :not_found -> nil
-        end
-    end
-  end
-
-  @spec cache_through_read!(state(), table(), cache_key()) :: name_record()
-  def cache_through_read!(state, table, key) do
-    case cache_through_read(state, table, key) do
-      {:ok, record} -> record
-      nil -> raise("#{inspect(key)} not found in #{table}")
-    end
-  end
-
-  @spec deactivate_name(state(), Blocks.height(), Blocks.height(), Model.name()) :: state()
+  @spec deactivate_name(
+          state(),
+          Blocks.height(),
+          Blocks.height(),
+          Model.name(),
+          deactivate_stat()
+        ) :: state()
   def deactivate_name(
         state,
         deactivate_height,
         expiration,
-        Model.name(index: plain_name, owner: owner_pk) = m_name
+        Model.name(index: plain_name, owner: owner_pk) = m_name,
+        reason
       ) do
     m_exp = Model.expiration(index: {deactivate_height, plain_name})
     m_owner = Model.owner(index: {owner_pk, plain_name})
@@ -334,33 +261,41 @@ defmodule AeMdw.Db.Sync.Name do
       Model.owner_deactivation(index: {owner_pk, deactivate_height, plain_name})
 
     state
-    |> cache_through_delete_active(expiration, m_name)
-    |> cache_through_write(Model.InactiveName, m_name)
-    |> cache_through_write(Model.InactiveNameExpiration, m_exp)
-    |> cache_through_write(Model.InactiveNameOwner, m_owner)
-    |> cache_through_write(Model.InactiveNameOwnerDeactivation, m_owner_deactivation)
+    |> delete_active(expiration, m_name)
+    |> State.put(Model.InactiveName, m_name)
+    |> State.put(Model.InactiveNameExpiration, m_exp)
+    |> State.put(Model.InactiveNameOwner, m_owner)
+    |> State.put(Model.InactiveNameOwnerDeactivation, m_owner_deactivation)
+    |> State.inc_stat(reason)
   end
 
-  @spec cache_through_delete_inactive(state(), nil | Model.name()) :: state()
-  def cache_through_delete_inactive(state, nil), do: state
+  @spec delete_inactive(state(), nil | Model.name()) :: state()
+  def delete_inactive(state, nil), do: state
 
-  def cache_through_delete_inactive(
+  def delete_inactive(
         state,
         Model.name(index: plain_name, owner: owner_pk) = m_name
       ) do
     expire = Names.revoke_or_expire_height(m_name)
 
     state
-    |> cache_through_delete(Model.InactiveName, plain_name)
-    |> cache_through_delete(Model.InactiveNameOwner, {owner_pk, plain_name})
-    |> cache_through_delete(Model.InactiveNameExpiration, {expire, plain_name})
-    |> cache_through_delete(Model.InactiveNameOwnerDeactivation, {owner_pk, expire, plain_name})
+    |> State.delete(Model.InactiveName, plain_name)
+    |> State.delete(Model.InactiveNameOwner, {owner_pk, plain_name})
+    |> State.delete(Model.InactiveNameExpiration, {expire, plain_name})
+    |> State.delete(Model.InactiveNameOwnerDeactivation, {owner_pk, expire, plain_name})
   end
 
-  defp plain_name!(state, name_hash) do
-    {:ok, m_plain_name} = cache_through_read(state, Model.PlainName, name_hash)
-
-    Model.plain_name(m_plain_name, :value)
+  defp delete_active(
+         state,
+         expiration,
+         Model.name(index: plain_name, active: active_from, owner: owner_pk)
+       ) do
+    state
+    |> State.delete(Model.ActiveName, plain_name)
+    |> State.delete(Model.ActiveNameOwner, {owner_pk, plain_name})
+    |> State.delete(Model.ActiveNameActivation, {active_from, plain_name})
+    |> State.delete(Model.ActiveNameExpiration, {expiration, plain_name})
+    |> State.delete(Model.ActiveNameOwnerDeactivation, {owner_pk, expiration, plain_name})
   end
 
   defp pointee_key(ptr, {bi, txi_idx}) do
