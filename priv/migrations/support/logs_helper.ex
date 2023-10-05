fe179b21aaa34c8784fdaea27f6bc760527ae673
Rogerio Pontual <rppontual@gmail.com>
2023-10-05 18:06:36 +0100

    chore: credo warnings

--- priv/migrations/support/logs_helper.ex
+++ priv/migrations/support/logs_helper.ex
@@ -7,9 +7,18 @@ defmodule AeMdw.Migrations.LogsHelper do
 
   require Model
 
+  @typep hash :: AeMdw.Node.Db.hash()
+  @typep optional_txi :: non_neg_integer() | nil
+  @typep optional_idx :: non_neg_integer() | nil
+  @typep event_boundary :: {
+    {hash(), optional_txi, optional_txi, optional_idx},
+    {hash(), optional_txi, optional_txi, optional_idx}
+  }
+
   @doc """
   Returns a list of boundaries for Model.EvtContractLog.
   """
+  @spec event_boundaries(State.t(), hash(), non_neg_integer()) :: list()
   def event_boundaries(state, evt_hash, max_range_len \\ 5_000) do
     case State.prev(state, Model.EvtContractLog, {evt_hash, nil, nil, nil}) do
       {:ok, {^evt_hash, last_txi, _create_txi, _idx}} ->
@@ -30,6 +39,7 @@ defmodule AeMdw.Migrations.LogsHelper do
   @doc """
   Streams Model.ContractLog records using Model.EvtContractLog boundaries.
   """
+  @spec stream_contract_logs_by_events(State.t(), event_boundary()) :: Enumerable.t()
   def stream_contract_logs_by_events(state, key_boundary) do
     state
     |> Collection.stream(Model.EvtContractLog, :forward, key_boundary, nil)
@@ -40,11 +50,13 @@ defmodule AeMdw.Migrations.LogsHelper do
   end
 
   defp txi_ranges(state, evt_hash, first_txi, last_txi, max_range_len) do
-    Stream.unfold(first_txi, fn range_first ->
-      if range_first do
+    Stream.unfold(first_txi, fn
+      nil -> nil
+      range_first ->
         case State.next(state, Model.EvtContractLog, {evt_hash, range_first, 0, 0}) do
           {:ok, {^evt_hash, next_txi, _create_txi, _idx}} ->
             range_last = min(next_txi + max_range_len, last_txi)
+            # credo:disable-for-next-line
             next = if range_last < last_txi, do: range_last + 1
 
             {range_first..range_last, next}
@@ -52,7 +64,6 @@ defmodule AeMdw.Migrations.LogsHelper do
           _no_transfer ->
             nil
         end
-      end
     end)
   end
 end
