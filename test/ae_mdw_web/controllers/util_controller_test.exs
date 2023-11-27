2fb1d0410d854f87844a14136d33fe4391d4c2c2
Sebastian Borrazas <seba.borrazas@gmail.com>
2023-11-27 10:55:40 -0300

    fix: use last gen status regardless of transaction index

--- test/ae_mdw_web/controllers/util_controller_test.exs
+++ test/ae_mdw_web/controllers/util_controller_test.exs
@@ -1,5 +1,10 @@
 defmodule AeMdwWeb.UtilControllerTest do
+  alias AeMdw.Db.Model
+  alias AeMdw.Db.Store
+
   use AeMdwWeb.ConnCase
+
+  require Model
 
   describe "static_file" do
     test "gets v1/v2 swagger files from priv directory", %{conn: conn} do
@@ -12,6 +17,20 @@ defmodule AeMdwWeb.UtilControllerTest do
                conn
                |> get("/v2/api")
                |> response(200)
+    end
+  end
+
+  describe "status" do
+    test "it returns last gen, regardless of transactions", %{conn: conn, store: store} do
+      store =
+        store
+        |> Store.put(Model.DeltaStat, Model.delta_stat(index: 10))
+
+      assert %{"mdw_height" => 10} =
+               conn
+               |> with_store(store)
+               |> get("/status")
+               |> json_response(200)
     end
   end
 end
