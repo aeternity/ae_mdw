8a9c8b098eeeaf2e68cdde80545f9e41de987cf2
Valentin Atanasov <valentin.atanasov@quanterall.com>
2024-05-09 11:27:48 +0300

    feat: add route for dex swaps by contract_id

--- lib/ae_mdw_web/controllers/dex_controller.ex
+++ lib/ae_mdw_web/controllers/dex_controller.ex
@@ -54,6 +54,24 @@ defmodule AeMdwWeb.DexController do
     end
   end
 
+  @spec swaps_for_contract(Conn.t(), map()) :: Conn.t()
+  def swaps_for_contract(%Conn{assigns: assigns} = conn, %{"contract_id" => contract_id}) do
+    %{state: state, pagination: pagination, cursor: cursor} = assigns
+
+    with {:ok, contract_pk} <- Validate.id(contract_id, [:contract_pubkey]),
+         {:ok, create_txi} <- validate_contract_pk(contract_pk, state, contract_id),
+         {:ok, swaps} <- Dex.fetch_contract_swaps(state, create_txi, pagination, cursor) do
+      Util.render(conn, swaps, &render_swap(state, &1))
+    end
+  end
+
+  defp validate_contract_pk(contract_pk, state, contract_id) do
+    case DexCache.get_contract_pk_txi(contract_pk, state) do
+      nil -> {:error, ErrInput.NotAex9.exception(value: contract_id)}
+      create_txi -> {:ok, create_txi}
+    end
+  end
+
   defp validate_token(token_symbol) do
     case DexCache.get_token_pair_txi(token_symbol) do
       nil -> {:error, ErrInput.NotAex9.exception(value: token_symbol)}
