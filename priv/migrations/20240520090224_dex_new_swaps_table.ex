66cf9af0427b7c93badfce3442fec378dff6aadb
Mihail Dobrev <mixaildobrev@gmail.com>
2024-05-27 17:34:04 +0300

    fix: chunk dex_swaps migration and fix pattern match (#1787)

--- priv/migrations/20240520090224_dex_new_swaps_table.ex
+++ priv/migrations/20240520090224_dex_new_swaps_table.ex
@@ -8,26 +8,27 @@ defmodule AeMdw.Migrations.DexNewSwapsTable do
   alias AeMdw.Db.Model
   alias AeMdw.Db.State
   alias AeMdw.Db.WriteMutation
-  alias AeMdw.Util
 
   require Model
 
   @spec run(State.t(), boolean()) :: {:ok, non_neg_integer()}
   def run(state, _from_start?) do
-    key_boundary = {{0, <<>>, 0, 0}, {nil, Util.max_256bit_bin(), nil, nil}}
-
-    mutations =
+    mutations_length =
       state
-      |> Collection.stream(Model.DexContractSwapTokens, :forward, key_boundary, nil)
-      |> Enum.map(fn Model.dex_contract_swap_tokens(index: {create_txi, _from, txi, idx}) ->
+      |> Collection.stream(Model.DexContractSwapTokens, nil)
+      |> Enum.map(fn {create_txi, _from, txi, idx} ->
         WriteMutation.new(
           Model.DexSwapTokens,
           Model.dex_swap_tokens(index: {create_txi, txi, idx})
         )
       end)
+      |> Stream.chunk_every(1000)
+      |> Stream.map(fn mutations ->
+        _state = State.commit_db(state, mutations)
+        length(mutations)
+      end)
+      |> Enum.sum()
 
-    _state = State.commit_db(state, mutations)
-
-    {:ok, length(mutations)}
+    {:ok, mutations_length}
   end
 end
