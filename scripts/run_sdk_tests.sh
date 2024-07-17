b02d9bf8333deb12223e9d9292165becca266f4b
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-07-17 14:50:09 +0400

    Fix docker warnings, use new `docker compose` (#1840)

--- scripts/run_sdk_tests.sh
+++ scripts/run_sdk_tests.sh
@@ -5,14 +5,14 @@ set -xe
 export AETERNITY_CONFIG=/home/aeternity/aeternity-dev.yaml
 export IMAGE_NAME_SUFFIX=devmode
 
-docker-compose -f docker-compose-dev.yml build --build-arg DEV_MODE=true ae_mdw
+docker compose -f docker-compose-dev.yml build --build-arg DEV_MODE=true ae_mdw
 
-docker-compose -f docker-compose-dev.yml up --detach ae_mdw --wait
+docker compose -f docker-compose-dev.yml up --detach ae_mdw --wait
 
-docker-compose -f docker-compose-dev.yml exec ae_mdw ./bin/ae_mdw rpc ':aeplugin_dev_mode_app.start_unlink()'
+docker compose -f docker-compose-dev.yml exec ae_mdw ./bin/ae_mdw rpc ':aeplugin_dev_mode_app.start_unlink()'
 
-docker-compose -f docker-compose-dev.yml run node_sdk /bin/sh -c "yarn install && node index.js"
+docker compose -f docker-compose-dev.yml run node_sdk /bin/sh -c "yarn install && node index.js"
 
-docker-compose -f docker-compose-dev.yml stop ae_mdw
+docker compose -f docker-compose-dev.yml stop ae_mdw
 
-docker-compose -f docker-compose-dev.yml run --workdir=/app ae_mdw scripts/test-devmode.sh
+docker compose -f docker-compose-dev.yml run --workdir=/app ae_mdw scripts/test-devmode.sh
