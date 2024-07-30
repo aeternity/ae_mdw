761b635fd0dc7989b6954ff7af4aa84bbddc506c
Sebastian Borrazas <seba.borrazas@gmail.com>
2024-07-30 07:37:30 -0300

    chore: fetch aexn tokens on render only (#1851)

--- lib/ae_mdw/aexn_tokens.ex
+++ lib/ae_mdw/aexn_tokens.ex
@@ -18,7 +18,6 @@ defmodule AeMdw.AexnTokens do
   alias AeMdw.Validate
 
   import AeMdw.Util.Encoding, only: [encode_contract: 1, encode_block: 2]
-  import AeMdwWeb.Helpers.AexnHelper, only: [sort_field_truncate: 1]
 
   require Model
 
@@ -53,7 +52,7 @@ defmodule AeMdw.AexnTokens do
           {:ok, aexn_contract()} | {:error, Error.t()}
   def fetch_contract(state, aexn_type, contract_id, v3?) do
     with {:ok, contract_pk} <- Validate.id(contract_id),
-         {:ok, aexn_contract} <- State.get(state, Model.AexnContract, {aexn_type, contract_pk}),
+         {:ok, aexn_contract} <- State.get(state, @aexn_table, {aexn_type, contract_pk}),
          {:invalid, false} <-
            {:invalid, State.exists?(state, Model.AexnInvalidContract, {aexn_type, contract_pk})} do
       {:ok, render_contract(state, aexn_contract, v3?)}
@@ -80,7 +79,7 @@ defmodule AeMdw.AexnTokens do
         ) ::
           {:ok, {cursor() | nil, [Model.aexn_contract()], cursor() | nil}} | {:error, Error.t()}
   def fetch_contracts(state, pagination, aexn_type, query, order_by, cursor, v3?) do
-    with {:ok, cursor} <- deserialize_aexn_cursor(cursor),
+    with {:ok, cursor} <- deserialize_aexn_cursor(order_by, cursor),
          {:ok, params} <- validate_params(query),
          {:ok, filters} <- Util.convert_params(params, &convert_param/1) do
       paginated_aexn_contracts =
@@ -89,7 +88,7 @@ defmodule AeMdw.AexnTokens do
         |> Collection.paginate(
           pagination,
           &render_contract(state, &1, v3?),
-          &serialize_aexn_cursor(order_by, &1)
+          &serialize_aexn_cursor/1
         )
 
       {:ok, paginated_aexn_contracts}
@@ -103,14 +102,7 @@ defmodule AeMdw.AexnTokens do
     }
 
     fn direction ->
-      state
-      |> Collection.stream(@aexn_creation_table, direction, key_boundary, cursor)
-      |> Stream.map(fn {aexn_type, txi_idx} ->
-        State.fetch!(state, @aexn_creation_table, {aexn_type, txi_idx})
-      end)
-      |> Stream.map(fn Model.aexn_contract_creation(contract_pk: pubkey) ->
-        State.fetch!(state, @aexn_table, {type, pubkey})
-      end)
+      Collection.stream(state, @aexn_creation_table, direction, key_boundary, cursor)
     end
   end
 
@@ -122,43 +114,26 @@ defmodule AeMdw.AexnTokens do
       {type, exact, Util.max_256bit_bin()}
     }
 
-    do_build_tokens_streamer(state, sorting_table, cursor, scope)
+    fn direction ->
+      Collection.stream(state, sorting_table, direction, scope, cursor)
+    end
   end
 
   defp build_tokens_streamer(%{prefix: prefix}, state, type, order_by, cursor) do
     sorting_table = Map.fetch!(@downcased_sorting_table, order_by)
-
-    cursor =
-      if not is_nil(cursor) do
-        {type, symbol_name, pubkey} = cursor
-        {type, String.downcase(symbol_name), pubkey}
-      end
-
-    prefix =
-      prefix
-      |> String.slice(0, @max_sort_field_length)
-      |> String.downcase()
 
     scope = {
       {type, prefix, <<>>},
       {type, prefix <> Util.max_name_bin(), <<>>}
     }
 
-    do_build_tokens_streamer(state, sorting_table, cursor, scope)
+    fn direction ->
+      Collection.stream(state, sorting_table, direction, scope, cursor)
+    end
   end
 
   defp build_tokens_streamer(_filters, state, type, table, cursor),
     do: build_tokens_streamer(%{prefix: ""}, state, type, table, cursor)
-
-  defp do_build_tokens_streamer(state, table, cursor, scope) do
-    fn direction ->
-      state
-      |> Collection.stream(table, direction, scope, cursor)
-      |> Stream.map(fn {type, _order_by_field, pubkey} ->
-        State.fetch!(state, @aexn_table, {type, pubkey})
-      end)
-    end
-  end
 
   defp validate_params(%{"prefix" => _prefix, "exact" => _exact}),
     do:
@@ -166,39 +141,33 @@ defmodule AeMdw.AexnTokens do
 
   defp validate_params(params), do: {:ok, params}
 
-  defp convert_param({"prefix", prefix}) when is_binary(prefix), do: {:ok, {:prefix, prefix}}
+  defp convert_param({"prefix", prefix}) when is_binary(prefix) do
+    prefix =
+      prefix
+      |> String.slice(0, @max_sort_field_length)
+      |> String.downcase()
+
+    {:ok, {:prefix, prefix}}
+  end
 
   defp convert_param({"exact", field_value}) when is_binary(field_value),
     do: {:ok, {:exact, field_value}}
 
   defp convert_param(other_param), do: {:error, ErrInput.Query.exception(value: other_param)}
 
-  defp serialize_aexn_cursor(
-         :creation,
-         Model.aexn_contract(index: {type, _pubkey}, txi_idx: txi_idx)
-       ) do
-    {type, txi_idx}
+  defp serialize_aexn_cursor(index) do
+    index
     |> :erlang.term_to_binary()
     |> Base.encode64(padding: false)
   end
 
-  defp serialize_aexn_cursor(
-         order_by,
-         Model.aexn_contract(index: {type, pubkey}, meta_info: meta_info)
-       ) do
-    sort_field_value = if order_by == :name, do: elem(meta_info, 0), else: elem(meta_info, 1)
+  defp deserialize_aexn_cursor(_order_by, nil), do: {:ok, nil}
 
-    {type, sort_field_truncate(sort_field_value), pubkey}
-    |> :erlang.term_to_binary()
-    |> Base.encode64(padding: false)
-  end
-
-  defp deserialize_aexn_cursor(nil), do: {:ok, nil}
-
-  defp deserialize_aexn_cursor(cursor_bin64) do
+  defp deserialize_aexn_cursor(:creation, cursor_bin64) do
     with {:ok, cursor_bin} <- Base.decode64(cursor_bin64, padding: false),
-         cursor_term <- :erlang.binary_to_term(cursor_bin),
-         true <- is_valid_cursor_term?(cursor_term) do
+         {aexn_type, {txi, idx}} = cursor_term
+         when aexn_type in ~w(aex9 aex141)a and is_integer(txi) and is_integer(idx) <-
+           :erlang.binary_to_term(cursor_bin) do
       {:ok, cursor_term}
     else
       _invalid ->
@@ -208,16 +177,29 @@ defmodule AeMdw.AexnTokens do
     ArgumentError -> {:error, ErrInput.Cursor.exception(value: cursor_bin64)}
   end
 
-  defp is_valid_cursor_term?({type, name_symbol_creation, pubkey})
-       when type in [:aex9, :aex141] and is_binary(name_symbol_creation) do
-    match?({:ok, _pk}, Validate.id(pubkey, [:contract_pubkey]))
+  defp deserialize_aexn_cursor(_order_by, cursor_bin64) do
+    with {:ok, cursor_bin} <- Base.decode64(cursor_bin64, padding: false),
+         {aexn_type, _name_symbol_creation, pubkey} = cursor_term
+         when aexn_type in ~w(aex9 aex141)a and is_binary(pubkey) <-
+           :erlang.binary_to_term(cursor_bin) do
+      {:ok, cursor_term}
+    else
+      _invalid ->
+        {:error, ErrInput.Cursor.exception(value: cursor_bin64)}
+    end
+  rescue
+    ArgumentError -> {:error, ErrInput.Cursor.exception(value: cursor_bin64)}
   end
 
-  defp is_valid_cursor_term?({type, {txi, idx}}) when type in [:aex9, :aex141] do
-    txi > 0 and idx >= -1
-  end
+  defp render_contract(state, {aexn_type, _symbol, contract_pk}, v3?),
+    do: render_contract(state, State.fetch!(state, @aexn_table, {aexn_type, contract_pk}), v3?)
 
-  defp is_valid_cursor_term?(_other_term), do: false
+  defp render_contract(state, {aexn_type, txi_idx}, v3?) do
+    Model.aexn_contract_creation(contract_pk: contract_pk) =
+      State.fetch!(state, @aexn_creation_table, {aexn_type, txi_idx})
+
+    render_contract(state, State.fetch!(state, @aexn_table, {aexn_type, contract_pk}), v3?)
+  end
 
   defp render_contract(
          state,
