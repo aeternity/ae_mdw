b8ef4a989ad2541b3dc4440ff7cec09ba444e99d
Mihail Dobrev <mixaildobrev@gmail.com>
2024-09-05 19:14:43 +0300

    fix: make timeout infinity

--- priv/migrations/20240905143020_create_claim_calls_table.ex
+++ priv/migrations/20240905143020_create_claim_calls_table.ex
@@ -29,40 +29,43 @@ defmodule AeMdw.Migrations.CreateClaimCallsTable do
       |> Stream.chunk_every(chunk_size)
       |> Task.async_stream(fn chunk ->
         chunk
-        |> Task.async_stream(fn {_tx_type, _tx_field_pos, account_pk, tx_index} ->
-          Model.tx(id: tx_hash, block_index: {height, _mbi}) =
-            State.fetch!(state, Model.Tx, tx_index)
+        |> Task.async_stream(
+          fn {_tx_type, _tx_field_pos, account_pk, tx_index} ->
+            Model.tx(id: tx_hash, block_index: {height, _mbi}) =
+              State.fetch!(state, Model.Tx, tx_index)
 
-          {:name_claim_tx, aetx} = Db.get_tx(tx_hash)
-          name = :aens_claim_tx.name(aetx)
+            {:name_claim_tx, aetx} = Db.get_tx(tx_hash)
+            name = :aens_claim_tx.name(aetx)
 
-          boundaries =
-            Collection.generate_key_boundary({name, height, {tx_index, Collection.integer()}})
+            boundaries =
+              Collection.generate_key_boundary({name, height, {tx_index, Collection.integer()}})
 
-          [
-            Collection.stream(
-              state,
-              Model.NameClaim,
-              :backward,
-              boundaries,
-              nil
-            ),
-            Collection.stream(
-              state,
-              Model.AuctionBidClaim,
-              :backward,
-              boundaries,
-              nil
-            )
-          ]
-          |> Collection.merge(:backward)
-          |> Enum.map(fn {plain_name, height, call_idx} ->
-            WriteMutation.new(
-              Model.ClaimCall,
-              Model.claim_call(index: {account_pk, call_idx, plain_name, height})
-            )
-          end)
-        end)
+            [
+              Collection.stream(
+                state,
+                Model.NameClaim,
+                :backward,
+                boundaries,
+                nil
+              ),
+              Collection.stream(
+                state,
+                Model.AuctionBidClaim,
+                :backward,
+                boundaries,
+                nil
+              )
+            ]
+            |> Collection.merge(:backward)
+            |> Enum.map(fn {plain_name, height, call_idx} ->
+              WriteMutation.new(
+                Model.ClaimCall,
+                Model.claim_call(index: {account_pk, call_idx, plain_name, height})
+              )
+            end)
+          end,
+          timeout: :infinity
+        )
         |> Enum.map(fn {:ok, mutations} -> mutations end)
       end)
       |> Enum.reduce(0, fn {:ok, mutations}, count ->
