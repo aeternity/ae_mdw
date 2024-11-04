4abfc64ff29baeadb4e0a89d8267309173253705
Valentin Atanasov <valentin.atanasov@quanterall.com>
2024-11-04 14:12:57 +0200

    fix: simplify getting info

--- lib/ae_mdw/hyperchain.ex
+++ lib/ae_mdw/hyperchain.ex
@@ -33,22 +33,17 @@ defmodule AeMdw.Hyperchain do
 
   @spec epoch_info_at_height(Blocks.height()) :: {:ok, epoch_info()} | :error
   def epoch_info_at_height(height) do
-    with {:ok, kb_hash} <- :aec_chain_state.get_key_block_hash_at_height(height),
-         {_tx_env, _trees} = run_env <-
-           :aetx_env.tx_env_and_trees_from_hash(:aetx_transaction, kb_hash),
-         {:ok, epoch} <- :aec_chain_hc.epoch(run_env) do
-      :aec_chain_hc.epoch_info_for_epoch(run_env, epoch)
+    with {:ok, epoch} <- :aec_chain_hc.epoch(height) do
+      :aec_chain_hc.epoch_info_for_epoch(height, epoch)
     end
   end
 
   @spec leaders_for_epoch_at_height(Blocks.height()) :: [{Blocks.height(), leader()}]
   def leaders_for_epoch_at_height(height) do
-    {:ok, kb_hash} = :aec_chain_state.get_key_block_hash_at_height(height)
-    {_tx_env, _trees} = run_env = :aetx_env.tx_env_and_trees_from_hash(:aetx_transaction, kb_hash)
-    {:ok, epoch} = :aec_chain_hc.epoch(run_env)
+    {:ok, epoch} = :aec_chain_hc.epoch(height)
 
     {:ok, %{seed: seed, validators: validators, length: length, first: first} = _epoch_info} =
-      :aec_chain_hc.epoch_info_for_epoch(run_env, epoch)
+      :aec_chain_hc.epoch_info_for_epoch(height, epoch)
 
     {:ok, seed} =
       case seed do
@@ -59,7 +54,7 @@ defmodule AeMdw.Hyperchain do
           {:ok, otherwise}
       end
 
-    {:ok, schedule} = :aec_chain_hc.validator_schedule(run_env, seed, validators, length)
+    {:ok, schedule} = :aec_chain_hc.validator_schedule(height, seed, validators, length)
 
     first
     |> Stream.iterate(fn x -> x + 1 end)
