03a2e7aa23101c6d0f27b054341148b5529778c8
Mihail Dobrev <mixaildobrev@gmail.com>
2024-11-26 17:08:24 +0200

    feat: add scoping to leaders endpoint

--- lib/ae_mdw_web/controllers/hyperchain_controller.ex
+++ lib/ae_mdw_web/controllers/hyperchain_controller.ex
@@ -1,8 +1,10 @@
 defmodule AeMdwWeb.HyperchainController do
-  alias AeMdw.Validate
   use AeMdwWeb, :controller
 
+  alias AeMdw.Error.Input, as: ErrInput
   alias AeMdw.Hyperchain
+  alias AeMdw.Node
+  alias AeMdw.Validate
   alias AeMdwWeb.FallbackController
   alias AeMdwWeb.Util, as: WebUtil
   alias AeMdwWeb.Plugs.HyperchainPlug
@@ -18,9 +20,11 @@ defmodule AeMdwWeb.HyperchainController do
     %{state: state, pagination: pagination, cursor: cursor, scope: scope} =
       assigns
 
-    state
-    |> Hyperchain.fetch_leaders(pagination, scope, cursor)
-    |> then(&WebUtil.render(conn, &1))
+    with {:ok, deserialized_scope} <- deserialize_leaders_scope(scope) do
+      state
+      |> Hyperchain.fetch_leaders(pagination, deserialized_scope, cursor)
+      |> then(&WebUtil.render(conn, &1))
+    end
   end
 
   @spec leader_by_height(Conn.t(), map()) :: Conn.t()
@@ -43,6 +47,26 @@ defmodule AeMdwWeb.HyperchainController do
   def validator(%Conn{assigns: %{state: state}} = conn, %{"validator_id" => validator_id}) do
     with {:ok, validator} <- Hyperchain.fetch_validator(state, validator_id) |> IO.inspect() do
       format_json(conn, validator)
+    end
+  end
+
+  defp deserialize_leaders_scope(scope) do
+    case scope do
+      nil ->
+        {:ok, nil}
+
+      {:gen, first_gen..last_gen//_step} ->
+        {:ok, {first_gen, last_gen}}
+
+      {:epoch, first_epoch..last_epoch//_step} ->
+        with {:ok, epoch_length} = Node.epoch_length(last_epoch),
+             {:ok, first_gen} <- Node.epoch_start_height(first_epoch),
+             {:ok, last_gen} <- Node.epoch_start_height(last_epoch) do
+          {:ok, {first_gen, last_gen + epoch_length - 1}}
+        else
+          {:error, error} ->
+            {:error, ErrInput.Scope.exception(value: error)}
+        end
     end
   end
 end
