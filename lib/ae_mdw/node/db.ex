58f3c55e5ed34a34531ff6779a31753940c301c8
Mihail Dobrev <mixaildobrev@gmail.com>
2024-12-03 16:32:28 +0200

    fix: pass extra argument to function from the node (#2028)

--- lib/ae_mdw/node/db.ex
+++ lib/ae_mdw/node/db.ex
@@ -290,6 +290,7 @@ defmodule AeMdw.Node.Db do
     {:value, micro_block} = :aec_db.find_block(mb_hash)
 
     node = :aec_chain_state.wrap_block(micro_block)
+    height = :aec_blocks.height(micro_block)
     prev_hash = :aec_block_insertion.node_prev_hash(node)
 
     {:value, trees_in, _tree, _difficulty, _fees, _fraud} =
@@ -300,8 +301,8 @@ defmodule AeMdw.Node.Db do
       |> :aec_blocks.to_header()
       |> :aec_headers.consensus_module()
 
-    node
-    |> consensus_mod.state_pre_transform_micro_node(trees_in)
+    height
+    |> consensus_mod.state_pre_transform_micro_node(node, trees_in)
     |> :aec_trees.accounts()
   end
 
