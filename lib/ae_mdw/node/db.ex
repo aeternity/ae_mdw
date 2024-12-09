ee25f20f3feea70da6d419189d0b7541ec6c09cb
Mihail Dobrev <mixaildobrev@gmail.com>
2024-12-09 17:47:08 +0200

    fix: add separate way to sync for hc

--- lib/ae_mdw/node/db.ex
+++ lib/ae_mdw/node/db.ex
@@ -34,57 +34,78 @@ defmodule AeMdw.Node.Db do
         ]
   def get_blocks_per_height(from_height, block_hash_or_height) do
     if Hyperchain.hyperchain?() do
-      get_blocks_per_height(from_height, block_hash_or_height, true)
+      get_blocks_per_height_hyperchain(from_height, block_hash_or_height)
     else
-      get_blocks_per_height(from_height, block_hash_or_height, false)
+      blocks_per_height(from_height, block_hash_or_height)
     end
   end
 
-  defp get_blocks_per_height(from_height, block_hash, is_hyperchain) when is_binary(block_hash),
-    do: get_blocks_per_height(from_height, block_hash, nil, is_hyperchain)
+  defp blocks_per_height(from_height, block_hash) when is_binary(block_hash),
+    do: blocks_per_height(from_height, block_hash, nil)
 
-  defp get_blocks_per_height(from_height, to_height, is_hyperchain) when is_integer(to_height) do
-    {:ok, header} = :aec_chain.get_key_header_by_height(to_height)
+  defp blocks_per_height(from_height, to_height) when is_integer(to_height) do
+    {:ok, header} = :aec_chain.get_key_header_by_height(to_height + 1)
 
     last_mb_hash = :aec_headers.prev_hash(header)
     {:ok, last_kb_hash} = :aec_headers.hash_header(header)
 
-    get_blocks_per_height(from_height, last_mb_hash, last_kb_hash, is_hyperchain)
+    blocks_per_height(from_height, last_mb_hash, last_kb_hash)
   end
 
-  defp get_blocks_per_height(from_height, last_mb_hash, last_kb_hash, is_hyperchain) do
+  defp blocks_per_height(from_height, last_mb_hash, last_kb_hash) do
     {:ok, root_hash} =
       :aec_chain.genesis_block()
       |> :aec_blocks.to_header()
       |> :aec_headers.hash_header()
 
     {last_mb_hash, last_kb_hash}
-    |> unfold_blocks(root_hash, is_hyperchain)
+    |> unfold_blocks(root_hash)
     |> Enum.take_while(fn {key_block, _micro_blocks, _last_kb_hash} ->
       :aec_blocks.height(key_block) >= from_height
     end)
     |> Enum.reverse()
   end
 
-  defp unfold_blocks(hashes, root_hash, true) do
-    Stream.unfold(hashes, fn
-      {_last_mb_hash, nil} ->
+  defp get_blocks_per_height_hyperchain(from_height, to_height) when is_integer(to_height) do
+    {:ok, header} = :aec_chain.get_key_header_by_height(to_height + 1)
+    {:ok, last_kb_hash} = :aec_headers.hash_header(header)
+
+    get_blocks_per_height_hyperchain(from_height, last_kb_hash)
+  end
+
+  defp get_blocks_per_height_hyperchain(from_height, last_kb_hash) when is_binary(last_kb_hash) do
+    {:ok, root_hash} =
+      :aec_chain.genesis_block()
+      |> :aec_blocks.to_header()
+      |> :aec_headers.hash_header()
+
+    last_kb_hash
+    |> unfold_blocks_hyperchain(root_hash)
+    |> Enum.take_while(fn {key_block, _micro_blocks, _last_kb_hash} ->
+      :aec_blocks.height(key_block) >= from_height
+    end)
+    |> Enum.reverse()
+  end
+
+  defp unfold_blocks_hyperchain(last_kb_hash, root_hash) do
+    Stream.unfold(last_kb_hash, fn
+      ^root_hash ->
         nil
 
-      {_last_mb_hash, ^root_hash} ->
-        root_block = :aec_db.get_block(root_hash)
-        {{root_block, [], root_hash}, {nil, nil}}
+      last_kb_hash ->
+        prev_key_hash =
+          last_kb_hash
+          |> :aec_db.get_block()
+          |> :aec_blocks.prev_key_hash()
 
-      {last_mb_hash, last_kb_hash} ->
-        {key_block, micro_blocks} = get_kb_mbs(last_mb_hash)
-        prev_hash = :aec_blocks.prev_hash(key_block)
-        prev_key_hash = :aec_blocks.prev_key_hash(key_block)
+        {:ok, %{key_block: prev_key_block, micro_blocks: micro_blocks}} =
+          :aec_chain.get_generation_by_hash(prev_key_hash, :backward)
 
-        {{key_block, micro_blocks, last_kb_hash}, {prev_hash, prev_key_hash}}
+        {{prev_key_block, micro_blocks, last_kb_hash}, prev_key_hash}
     end)
   end
 
-  defp unfold_blocks(hashes, root_hash, false) do
+  defp unfold_blocks(hashes, root_hash) do
     Stream.unfold(hashes, fn
       {_last_mb_hash, ^root_hash} ->
         nil
