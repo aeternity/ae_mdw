8868e72488d2bcc3176bda83a856d37f559c507f
Mihail Dobrev <mixaildobrev@gmail.com>
2025-04-22 13:13:42 +0300

    fix: reverse migrations order to avoid data loss

--- priv/migrations/20250411140928_initial_count_of_account_counters.ex
+++
@@ -1,7 +0,0 @@
-defmodule AeMdw.Migrations.InitialCountOfAccountCounters do
-  alias AeMdw.Db.State
-
-  @spec run(State.t(), boolean()) :: {:ok, non_neg_integer()}
-  def run(_state, _from_start?) do
-  end
-end
