402dc90fbfef0c5bf9132f550a0e7115cdeaba2f
Gaith Hallak <gaithhallak@gmail.com>
2025-11-06 12:53:55 +0300

    Fix single transaction query

--- lib/ae_mdw_web/graphql/schema/queries/transaction_queries.ex
+++ lib/ae_mdw_web/graphql/schema/queries/transaction_queries.ex
@@ -2,43 +2,43 @@ defmodule AeMdwWeb.GraphQL.Schema.Queries.TransactionQueries do
   use Absinthe.Schema.Notation
 
   object :transaction_queries do
-    @desc "Fetch a transaction by hash (or numeric index as string)"
+    @desc "Get a single transaction"
     field :transaction, :transaction do
-      arg(:id, non_null(:string), description: "Transaction hash or numeric index")
+      arg(:hash, non_null(:string))
       resolve(&AeMdwWeb.GraphQL.Resolvers.TransactionResolver.transaction/3)
     end
 
-    @desc "Count transactions (optionally filtered)"
-    field :transactions_count, :integer do
-      arg(:from_txi, :integer)
-      arg(:to_txi, :integer)
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
-      arg(:account, :string)
-      arg(:type, :string)
-      arg(:filter, :transaction_filter)
-      resolve(&AeMdwWeb.GraphQL.Resolvers.TransactionResolver.transactions_count/3)
-    end
+    #@desc "Total number of transactions"
+    #field :transactions_count, :integer do
+    #  arg(:from_txi, :integer)
+    #  arg(:to_txi, :integer)
+    #  arg(:from_height, :integer)
+    #  arg(:to_height, :integer)
+    #  arg(:account, :string)
+    #  arg(:type, :string)
+    #  arg(:filter, :transaction_filter)
+    #  resolve(&AeMdwWeb.GraphQL.Resolvers.TransactionResolver.transactions_count/3)
+    #end
 
-    @desc "Paginated transactions (backward direction); minimal filters supported"
-    field :transactions, :transaction_page do
-      arg(:cursor, :string, description: "Opaque cursor (tx index)")
-      arg(:limit, :integer, default_value: 20)
-      # legacy txi range (still accepted)
-      arg(:from_txi, :integer)
-      arg(:to_txi, :integer)
-      # new height based range
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
-      arg(:account, :string, description: "Account public key filter")
-      arg(:type, :string, description: "Transaction type filter (e.g. spend_tx)")
+    #@desc "Paginated transactions (backward direction); minimal filters supported"
+    #field :transactions, :transaction_page do
+    #  arg(:cursor, :string, description: "Opaque cursor (tx index)")
+    #  arg(:limit, :integer, default_value: 20)
+    #  # legacy txi range (still accepted)
+    #  arg(:from_txi, :integer)
+    #  arg(:to_txi, :integer)
+    #  # new height based range
+    #  arg(:from_height, :integer)
+    #  arg(:to_height, :integer)
+    #  arg(:account, :string, description: "Account public key filter")
+    #  arg(:type, :string, description: "Transaction type filter (e.g. spend_tx)")
 
-      arg(:filter, :transaction_filter,
-        description: "Compound filter object {account,type,from_height,to_height}"
-      )
+    #  arg(:filter, :transaction_filter,
+    #    description: "Compound filter object {account,type,from_height,to_height}"
+    #  )
 
-      resolve(&AeMdwWeb.GraphQL.Resolvers.TransactionResolver.transactions/3)
-    end
+    #  resolve(&AeMdwWeb.GraphQL.Resolvers.TransactionResolver.transactions/3)
+    #end
 
     @desc "Transactions contained in a micro block"
     field :micro_block_transactions, :transaction_page do
--- lib/ae_mdw_web/graphql/schema/types/transaction_types.ex
+++ lib/ae_mdw_web/graphql/schema/types/transaction_types.ex
@@ -7,31 +7,20 @@ defmodule AeMdwWeb.GraphQL.Schema.Types.TransactionTypes do
   Macros.page(:transaction)
 
   object :transaction do
-    field(:hash, :string)
     field(:block_hash, :string)
     field(:block_height, :integer)
+    field(:encoded_tx, :string)
+    field(:hash, :string)
     field(:micro_index, :integer)
     field(:micro_time, :integer)
-    field(:tx_index, :integer)
     field(:signatures, list_of(:string))
-    field(:tx, :string, description: "Underlying tx object encoded as JSON string")
-    # enrichment (common tx metadata)
-    field(:fee, :integer)
-    field(:type, :string)
-    field(:gas, :integer)
-    field(:gas_price, :integer)
-    field(:nonce, :integer)
-    field(:sender_id, :string)
-    field(:recipient_id, :string)
-    field(:amount, :integer)
-    field(:ttl, :integer)
-    field(:payload, :string)
+    field(:tx, :json)
   end
 
-  input_object :transaction_filter do
-    field(:account, :string)
-    field(:type, :string)
-    field(:from_height, :integer)
-    field(:to_height, :integer)
-  end
+  #input_object :transaction_filter do
+  #  field(:account, :string)
+  #  field(:type, :string)
+  #  field(:from_height, :integer)
+  #  field(:to_height, :integer)
+  #end
 end
