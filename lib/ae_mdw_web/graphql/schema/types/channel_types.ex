b0c3dccb1cfaae0d907e9f3fffeac6cc75232c60
Gaith Hallak <gaithhallak@gmail.com>
2025-11-06 16:27:11 +0300

    fix: all channel queries

--- lib/ae_mdw_web/graphql/schema/types/channel_types.ex
+++ lib/ae_mdw_web/graphql/schema/types/channel_types.ex
@@ -12,29 +12,35 @@ defmodule AeMdwWeb.GraphQL.Schema.Types.ChannelTypes do
   Macros.page(:channel)
 
   object :channel do
+    field(:active, :boolean)
     field(:channel, :string)
-    field(:initiator, :string)
+    field(:amount, :big_int)
     field(:responder, :string)
+    field(:initiator, :string)
     field(:state_hash, :string)
     field(:last_updated_height, :integer)
     field(:last_updated_time, :integer)
     field(:last_updated_tx_hash, :string)
     field(:last_updated_tx_type, :string)
     field(:updates_count, :integer)
-    field(:active, :boolean)
-    field(:amount, :big_int)
-    # Extra node details if available (balances, deposits, etc.) are flattened into this object
+    field(:channel_reserve, :big_int)
+    field(:delegate_ids, :json)
+    field(:initiator_amount, :big_int)
+    field(:lock_period, :integer)
+    field(:locked_until, :integer)
+    field(:responder_amount, :big_int)
     field(:round, :integer)
+    field(:solo_round, :integer)
   end
 
   Macros.page(:channel_update)
 
   object :channel_update do
     field(:channel, :string)
-    field(:tx_type, :string)
     field(:block_hash, :string)
+    field(:tx, :json)
+    field(:tx_type, :string)
     field(:source_tx_hash, :string)
     field(:source_tx_type, :string)
-    field(:tx, :json)
   end
 end
