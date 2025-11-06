d837ec451b00632d293dba15f932f2c0716e1ecb
Gaith Hallak <gaithhallak@gmail.com>
2025-11-06 16:27:11 +0300

    Fix all channel queries

--- lib/ae_mdw_web/graphql/resolvers/channel_resolver.ex
+++ lib/ae_mdw_web/graphql/resolvers/channel_resolver.ex
@@ -1,29 +1,19 @@
 defmodule AeMdwWeb.GraphQL.Resolvers.ChannelResolver do
-  @moduledoc """
-  Channel resolvers: list channels, fetch single channel (optionally at a given block), and channel updates.
-  """
-  alias AeMdw.{Channels, Validate}
-  alias AeMdw.Db.State
+  alias AeMdw.Channels
+  alias AeMdw.Validate
   alias AeMdw.Error.Input, as: ErrInput
+  alias AeMdwWeb.GraphQL.Resolvers.Helpers
 
-  @max_limit 100
-
-  # -------------- Channels list --------------
-  @spec channels(any, map(), Absinthe.Resolution.t()) :: {:ok, map()} | {:error, String.t()}
-  def channels(_p, args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
-    cursor = Map.get(args, :cursor)
+  def channels(_p, args, %{context: %{state: state}}) do
     state_filter = Map.get(args, :state)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
-
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
+    cursor = Map.get(args, :cursor)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
     query =
       case state_filter do
@@ -32,104 +22,51 @@ defmodule AeMdwWeb.GraphQL.Resolvers.ChannelResolver do
         v -> %{"state" => to_string(v)}
       end
 
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Channels.fetch_channels(state, pagination, range, query, cursor) do
+    case Channels.fetch_channels(state, pagination, scope, query, cursor) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.Query{}} ->
-        {:error, "invalid_filter"}
-
-      {:error, _} ->
-        {:error, "channels_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def channels(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Single channel --------------
-  @spec channel(any, map(), Absinthe.Resolution.t()) :: {:ok, map()} | {:error, String.t()}
-  def channel(_p, %{id: id} = args, %{context: %{state: %State{} = state}}) do
-    block_hash = Map.get(args, :block_hash)
-
+  def channel(_p, %{id: id}, %{context: %{state: state}}) do
     with {:ok, channel_pk} <- Validate.id(id, [:channel]),
-         {:ok, type_block_hash} <- parse_optional_block_hash(block_hash),
-         {:ok, channel} <- Channels.fetch_channel(state, channel_pk, type_block_hash) do
+         {:ok, channel} <- Channels.fetch_channel(state, channel_pk, nil) do
       {:ok, channel}
     else
-      {:error, %ErrInput.Id{}} -> {:error, "invalid_channel_id"}
-      {:error, %ErrInput.NotFound{}} -> {:error, "channel_not_found"}
-      {:error, _} -> {:error, "channel_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def channel(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Channel updates --------------
-  @spec channel_updates(any, map(), Absinthe.Resolution.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def channel_updates(_p, %{id: id} = args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
+  def channel_updates(_p, %{id: id} = args, %{context: %{state: state}}) do
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
-
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Channels.fetch_channel_updates(state, id, pagination, range, cursor) do
+    case Channels.fetch_channel_updates(state, id, pagination, scope, cursor) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.NotFound{}} ->
-        {:error, "channel_not_found"}
-
-      {:error, _} ->
-        {:error, "channel_updates_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
-
-  def channel_updates(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Helpers --------------
-  defp parse_optional_block_hash(nil), do: {:ok, nil}
-
-  defp parse_optional_block_hash(block_hash) when is_binary(block_hash) do
-    with {:ok, decoded} <- Validate.id(block_hash) do
-      if String.starts_with?(block_hash, "kh") do
-        {:ok, {:key, decoded}}
-      else
-        {:ok, {:micro, decoded}}
-      end
-    else
-      _ -> {:error, ErrInput.Id.exception(value: block_hash)}
-    end
-  end
-
-  defp cursor_val(nil), do: nil
-  defp cursor_val({val, _rev}), do: val
-
-  defp clamp_limit(l) when is_integer(l) and l > @max_limit, do: @max_limit
-  defp clamp_limit(l) when is_integer(l) and l > 0, do: l
-  defp clamp_limit(_), do: 20
 end
--- lib/ae_mdw_web/graphql/schema/queries/channel_queries.ex
+++ lib/ae_mdw_web/graphql/schema/queries/channel_queries.ex
@@ -2,30 +2,37 @@ defmodule AeMdwWeb.GraphQL.Schema.Queries.ChannelQueries do
   use Absinthe.Schema.Notation
 
   object :channel_queries do
-    @desc "Paginated channels; supports state filter and optional generation range"
+    @desc "Get multiple channels"
     field :channels, :channel_page do
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
       arg(:state, :channel_state)
+
+      # Pagination args
+      arg(:cursor, :string)
+      arg(:limit, :integer)
+      arg(:direction, :direction, default_value: :backward)
       arg(:from_height, :integer)
       arg(:to_height, :integer)
+
       resolve(&AeMdwWeb.GraphQL.Resolvers.ChannelResolver.channels/3)
     end
 
-    @desc "Fetch a single channel by id; optionally at a specific block hash"
+    @desc "Fetch a single channel"
     field :channel, :channel do
       arg(:id, non_null(:string))
-      arg(:block_hash, :string, description: "Key-block hash (kh_*) or micro-block hash (mh_*)")
       resolve(&AeMdwWeb.GraphQL.Resolvers.ChannelResolver.channel/3)
     end
 
-    @desc "Channel updates (nested transactions)"
+    @desc "Fetch all updates done to a channel"
     field :channel_updates, :channel_update_page do
       arg(:id, non_null(:string))
+
+      # Pagination args
       arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
+      arg(:limit, :integer)
+      arg(:direction, :direction, default_value: :backward)
       arg(:from_height, :integer)
       arg(:to_height, :integer)
+
       resolve(&AeMdwWeb.GraphQL.Resolvers.ChannelResolver.channel_updates/3)
     end
   end
