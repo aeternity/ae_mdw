7c6a6804df8a0aa471b2c722e6353798cbbb708b
Gaith Hallak <gaithhallak@gmail.com>
2025-11-06 14:17:32 +0300

    fix: all oracle queries

--- lib/ae_mdw_web/graphql/resolvers/oracle_resolver.ex
+++ lib/ae_mdw_web/graphql/resolvers/oracle_resolver.ex
@@ -1,46 +1,32 @@
 defmodule AeMdwWeb.GraphQL.Resolvers.OracleResolver do
-  @moduledoc """
-  Oracle resolvers: single oracle lookup and paginated lists for oracles, queries, responses, and extends.
-  """
-  alias AeMdw.{Oracles}
+  alias AeMdw.Oracles
   alias AeMdw.Db.State
   alias AeMdw.Error.Input, as: ErrInput
   alias AeMdwWeb.GraphQL.Resolvers.Helpers
 
-  # -------------- Single Oracle --------------
-  @spec oracle(any, map(), Absinthe.Resolution.t()) :: {:ok, map()} | {:error, String.t()}
   def oracle(_p, %{id: id}, %{context: %{state: %State{} = state}}) do
     case AeMdw.Validate.id(id, [:oracle_pubkey]) do
       {:ok, pk} ->
         case Oracles.fetch(state, pk, v3?: true) do
           {:ok, oracle} -> {:ok, oracle}
-          {:error, %ErrInput.NotFound{}} -> {:error, "oracle_not_found"}
-          {:error, _} -> {:error, "oracle_error"}
+          {:error, err} -> {:error, ErrInput.message(err)}
         end
 
-      {:error, _} ->
-        {:error, "invalid_oracle_id"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def oracle(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Oracles list --------------
-  @spec oracles(any, map(), Absinthe.Resolution.t()) :: {:ok, map()} | {:error, String.t()}
   def oracles(_p, args, %{context: %{state: %State{} = state}}) do
+    state_filter = Map.get(args, :state)
     limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    state_filter = args |> Map.get(:state)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
-
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
     query =
       case state_filter do
@@ -49,90 +35,67 @@ defmodule AeMdwWeb.GraphQL.Resolvers.OracleResolver do
         v -> %{"state" => to_string(v)}
       end
 
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Oracles.fetch_oracles(state, pagination, range, query, cursor, v3?: true) do
+    case Oracles.fetch_oracles(state, pagination, scope, query, cursor, [{:v3?, true}]) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: Helpers.cursor_val(prev), next_cursor: Helpers.cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.Query{}} ->
-        {:error, "invalid_filter"}
-
-      {:error, _} ->
-        {:error, "oracles_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def oracles(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Oracle Queries --------------
-  @spec oracle_queries(any, map(), Absinthe.Resolution.t()) :: {:ok, map()} | {:error, String.t()}
-  def oracle_queries(_p, %{id: id} = args, %{context: %{state: %State{} = state}}) do
+  def oracle_queries(_p, %{id: id} = args, %{context: %{state: state}}) do
     limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    pagination = {:backward, false, limit, not is_nil(cursor)}
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    case Oracles.fetch_oracle_queries(state, id, pagination, nil, cursor) do
+    case Oracles.fetch_oracle_queries(state, id, pagination, scope, cursor) do
       {:ok, {prev, queries, next}} ->
-        {:ok, %{prev_cursor: Helpers.cursor_val(prev), next_cursor: Helpers.cursor_val(next), data: queries}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: queries
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.NotFound{}} ->
-        {:error, "oracle_not_found"}
-
-      {:error, _} ->
-        {:error, "oracle_queries_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def oracle_queries(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Oracle Responses --------------
-  @spec oracle_responses(any, map(), Absinthe.Resolution.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def oracle_responses(_p, %{id: id} = args, %{context: %{state: %State{} = state}}) do
+  def oracle_responses(_p, %{id: id} = args, %{context: %{state: state}}) do
     limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
-
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Oracles.fetch_oracle_responses(state, id, pagination, range, cursor) do
+    case Oracles.fetch_oracle_responses(state, id, pagination, scope, cursor) do
       {:ok, {prev, responses, next}} ->
-        {:ok, %{prev_cursor: Helpers.cursor_val(prev), next_cursor: Helpers.cursor_val(next), data: responses}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: responses
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.NotFound{}} ->
-        {:error, "oracle_not_found"}
-
-      {:error, _} ->
-        {:error, "oracle_responses_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
-
-  def oracle_responses(_, _args, _), do: {:error, "partial_state_unavailable"}
 
   # -------------- Oracle Extends --------------
   # @spec oracle_extends(any, map(), Absinthe.Resolution.t()) :: {:ok, map()} | {:error, String.t()}
--- lib/ae_mdw_web/graphql/schema/queries/oracle_queries.ex
+++ lib/ae_mdw_web/graphql/schema/queries/oracle_queries.ex
@@ -2,37 +2,51 @@ defmodule AeMdwWeb.GraphQL.Schema.Queries.OracleQueries do
   use Absinthe.Schema.Notation
 
   object :oracle_queries do
+    @desc "Get multiple oracles"
+    field :oracles, :oracle_page do
+      arg(:state, :oracle_state)
+
+      # Pagination args
+      arg(:cursor, :string)
+      arg(:limit, :integer)
+      arg(:direction, :direction, default_value: :backward)
+      arg(:from_height, :integer)
+      arg(:to_height, :integer)
+
+      resolve(&AeMdwWeb.GraphQL.Resolvers.OracleResolver.oracles/3)
+    end
+
     @desc "Fetch an oracle by its public key"
     field :oracle, :oracle do
       arg(:id, non_null(:string))
       resolve(&AeMdwWeb.GraphQL.Resolvers.OracleResolver.oracle/3)
     end
 
-    @desc "Paginated oracles (backward by expiration); supports state filter and optional generation range"
-    field :oracles, :oracle_page do
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
-      arg(:state, :oracle_state, description: "Filter by oracle lifecycle state")
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
-      resolve(&AeMdwWeb.GraphQL.Resolvers.OracleResolver.oracles/3)
-    end
-
-    @desc "Oracle queries (questions addressed to the oracle)"
+    @desc "Get an oracle's queries"
     field :oracle_queries, :oracle_query_page do
       arg(:id, non_null(:string))
+
+      # Pagination args
       arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
+      arg(:limit, :integer)
+      arg(:direction, :direction, default_value: :backward)
+      arg(:from_height, :integer)
+      arg(:to_height, :integer)
+
       resolve(&AeMdwWeb.GraphQL.Resolvers.OracleResolver.oracle_queries/3)
     end
 
-    @desc "Oracle responses (answers provided by the oracle); supports optional generation range"
+    @desc "Get an oracle's responses"
     field :oracle_responses, :oracle_response_page do
       arg(:id, non_null(:string))
+
+      # Pagination args
       arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
+      arg(:limit, :integer)
+      arg(:direction, :direction, default_value: :backward)
       arg(:from_height, :integer)
       arg(:to_height, :integer)
+
       resolve(&AeMdwWeb.GraphQL.Resolvers.OracleResolver.oracle_responses/3)
     end
 
