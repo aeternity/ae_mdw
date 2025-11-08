5196cdd1eac012f1475e324061feaac55d889037
Gaith Hallak <gaithhallak@gmail.com>
2025-11-08 14:46:01 +0300

    Add query for account activities

--- lib/ae_mdw_web/graphql/schema/queries/account_queries.ex
+++ lib/ae_mdw_web/graphql/schema/queries/account_queries.ex
@@ -1,43 +1,18 @@
 defmodule AeMdwWeb.GraphQL.Schema.Queries.AccountQueries do
   use Absinthe.Schema.Notation
 
+  alias AeMdwWeb.GraphQL.Schema.Helpers.Macros
+
+  require Macros
+
   object :account_queries do
-    @desc "Fetch an account by its public key"
-    field :account, :account do
+    @desc "Account activities"
+    field :account_activities, :account_activity_page do
       arg(:id, non_null(:string))
-      resolve(&AeMdwWeb.GraphQL.Resolvers.AccountResolver.account/3)
+      arg(:owned_only, :boolean)
+      arg(:type, :activity_type)
+      Macros.pagination_args_with_scope()
+      resolve(&AeMdwWeb.GraphQL.Resolvers.AccountResolver.activities/3)
     end
-
-    @desc "Paginated accounts (backward by pubkey); only nextCursor supported for now"
-    field :accounts, :account_page do
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
-      resolve(&AeMdwWeb.GraphQL.Resolvers.AccountResolver.accounts/3)
-    end
-
-    @desc "Names owned by an account"
-    field :account_names, :name_page do
-      arg(:id, non_null(:string))
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 20)
-      resolve(&AeMdwWeb.GraphQL.Resolvers.AccountResolver.account_names/3)
-    end
-
-    @desc "AEX9 token balances for an account"
-    field :account_aex9_balances, :aex9_balance_page do
-      arg(:id, non_null(:string))
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 50)
-      resolve(&AeMdwWeb.GraphQL.Resolvers.AccountResolver.account_aex9_balances/3)
-    end
-
-    # @desc "AEX141 tokens owned by an account"
-    # field :account_aex141_tokens, :aex141_token_owner_page do
-    #  arg(:id, non_null(:string))
-    #  arg(:contract_id, :string, description: "Optional filter by contract id")
-    #  arg(:cursor, :string)
-    #  arg(:limit, :integer, default_value: 50)
-    #  resolve(&AeMdwWeb.GraphQL.Resolvers.AccountResolver.account_aex141_tokens/3)
-    # end
   end
 end
