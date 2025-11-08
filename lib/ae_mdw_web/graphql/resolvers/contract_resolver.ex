7cd8427f530e2184781e824daea86b7ab8d06ca3
Gaith Hallak <gaithhallak@gmail.com>
2025-11-08 11:22:58 +0300

    Fix contract queries

--- lib/ae_mdw_web/graphql/resolvers/contract_resolver.ex
+++ lib/ae_mdw_web/graphql/resolvers/contract_resolver.ex
@@ -1,243 +1,148 @@
 defmodule AeMdwWeb.GraphQL.Resolvers.ContractResolver do
-  @moduledoc """
-  Contract resolvers: contract list, single contract, logs (global and by contract), and calls (global and by contract).
-  """
-  alias AeMdw.{Contracts}
-  alias AeMdw.Db.State
+  alias AeMdw.Contracts
   alias AeMdw.Error.Input, as: ErrInput
+  alias AeMdwWeb.GraphQL.Resolvers.Helpers
 
-  @max_limit 100
-
-  def contract(_p, %{id: id}, %{context: %{state: %State{} = state}}) do
+  def contract(_p, %{id: id}, %{context: %{state: state}}) do
     case Contracts.fetch_contract(state, id) do
-      {:ok, contract} -> {:ok, contract}
-      {:error, %ErrInput.NotFound{}} -> {:error, "contract_not_found"}
-      {:error, _} -> {:error, "contract_error"}
+      {:ok, contract} -> {:ok, contract |> Helpers.normalize_map()}
+      {:error, err} -> {:error, ErrInput.message(err)}
     end
   end
 
-  def contract(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  def contracts(_p, args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
+  def contracts(_p, args, %{context: %{state: state}}) do
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
-
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Contracts.fetch_contracts(state, pagination, range, cursor) do
+    case Contracts.fetch_contracts(state, pagination, scope, cursor) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items |> Enum.map(&Helpers.normalize_map/1)
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, _} ->
-        {:error, "contracts_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def contracts(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  def logs(_p, args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
+  def logs(_p, args, %{context: %{state: state}}) do
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
+    query = %{}
+    query = Helpers.maybe_put(query, "contract_id", Map.get(args, :contract_id))
+    query = Helpers.maybe_put(query, "event", Map.get(args, :event))
+    query = Helpers.maybe_put(query, "function", Map.get(args, :function))
+    query = Helpers.maybe_put(query, "function_prefix", Map.get(args, :function_prefix))
+    query = Helpers.maybe_put(query, "data", Map.get(args, :data))
+    query = Helpers.maybe_put(query, "aexn_args", Map.get(args, :aexn_args))
 
-    query = build_logs_query(args)
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Contracts.fetch_logs(state, pagination, range, query, cursor, v3?: true) do
+    case Contracts.fetch_logs(state, pagination, scope, query, cursor, v3?: true) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items |> Enum.map(&Helpers.normalize_map/1)
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.Query{}} ->
-        {:error, "invalid_filter"}
-
-      {:error, _} ->
-        {:error, "contract_logs_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def logs(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  def contract_logs(_p, %{id: id} = args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
+  def calls(_p, args, %{context: %{state: state}}) do
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
-
-    query = build_logs_query(args)
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Contracts.fetch_contract_logs(state, id, pagination, range, query, cursor) do
+    case Contracts.fetch_calls(state, pagination, scope, [], cursor, v3?: true) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items |> Enum.map(&Helpers.normalize_map/1)
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.NotFound{}} ->
-        {:error, "contract_not_found"}
-
-      {:error, %ErrInput.Query{}} ->
-        {:error, "invalid_filter"}
-
-      {:error, _} ->
-        {:error, "contract_logs_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def contract_logs(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  def calls(_p, args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
+  def contract_logs(_p, %{id: id} = args, %{context: %{state: state}}) do
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
+    query = %{}
+    query = Helpers.maybe_put(query, "contract_id", Map.get(args, :contract_id))
+    query = Helpers.maybe_put(query, "event", Map.get(args, :event))
+    query = Helpers.maybe_put(query, "function", Map.get(args, :function))
+    query = Helpers.maybe_put(query, "function_prefix", Map.get(args, :function_prefix))
+    query = Helpers.maybe_put(query, "data", Map.get(args, :data))
+    query = Helpers.maybe_put(query, "aexn_args", Map.get(args, :aexn_args))
 
-    query = build_calls_query(args)
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Contracts.fetch_calls(state, pagination, range, query, cursor, v3?: true) do
+    case Contracts.fetch_contract_logs(state, id, pagination, scope, query, cursor) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items |> Enum.map(&Helpers.normalize_map/1)
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.Query{}} ->
-        {:error, "invalid_filter"}
-
-      {:error, _} ->
-        {:error, "contract_calls_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
 
-  def calls(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  def contract_calls(_p, %{id: id} = args, %{context: %{state: %State{} = state}}) do
-    limit = clamp_limit(Map.get(args, :limit, 20))
+  def contract_calls(_p, %{id: id} = args, %{context: %{state: state}}) do
+    limit = Helpers.clamp_page_limit(Map.get(args, :limit))
     cursor = Map.get(args, :cursor)
-    from_h = Map.get(args, :from_height)
-    to_h = Map.get(args, :to_height)
+    direction = Map.get(args, :direction, :backward)
+    from_height = Map.get(args, :from_height)
+    to_height = Map.get(args, :to_height)
+    # TODO: scoping does not work as expected
+    scope = Helpers.make_scope(from_height, to_height)
+    pagination = {direction, false, limit, not is_nil(cursor)}
 
-    range =
-      cond do
-        from_h && to_h -> {:gen, from_h..to_h}
-        to_h && is_nil(from_h) -> {:gen, 0..to_h}
-        from_h && is_nil(to_h) -> {:gen, from_h..from_h}
-        true -> nil
-      end
-
-    query = build_calls_query(args)
-    pagination = {:backward, false, limit, not is_nil(cursor)}
-
-    case Contracts.fetch_contract_calls(state, id, pagination, range, query, cursor) do
+    case Contracts.fetch_contract_calls(state, id, pagination, scope, [], cursor) do
       {:ok, {prev, items, next}} ->
-        {:ok, %{prev_cursor: cursor_val(prev), next_cursor: cursor_val(next), data: items}}
+        {:ok,
+         %{
+           prev_cursor: Helpers.cursor_val(prev),
+           next_cursor: Helpers.cursor_val(next),
+           data: items |> Enum.map(&Helpers.normalize_map/1)
+         }}
 
-      {:error, %ErrInput.Cursor{}} ->
-        {:error, "invalid_cursor"}
-
-      {:error, %ErrInput.Scope{}} ->
-        {:error, "invalid_scope"}
-
-      {:error, %ErrInput.NotFound{}} ->
-        {:error, "contract_not_found"}
-
-      {:error, %ErrInput.Query{}} ->
-        {:error, "invalid_filter"}
-
-      {:error, _} ->
-        {:error, "contract_calls_error"}
+      {:error, err} ->
+        {:error, ErrInput.message(err)}
     end
   end
-
-  def contract_calls(_, _args, _), do: {:error, "partial_state_unavailable"}
-
-  # -------------- Helpers --------------
-  defp build_logs_query(args) do
-    acc = %{}
-    acc = maybe_put(acc, "contract", Map.get(args, :contract_id))
-    acc = maybe_put(acc, "data", Map.get(args, :data_prefix))
-    acc = maybe_put(acc, "event", Map.get(args, :event))
-    acc = maybe_put(acc, "function", Map.get(args, :function))
-    acc = maybe_put(acc, "function_prefix", Map.get(args, :function_prefix))
-    # boolean flags must be encoded with expected keys
-    acc = maybe_put(acc, "aexn-args", encode_bool(Map.get(args, :aexn_args)))
-    acc = maybe_put(acc, "custom-args", encode_bool(Map.get(args, :custom_args)))
-    acc
-  end
-
-  defp build_calls_query(args) do
-    acc = %{}
-    acc = maybe_put(acc, "contract", Map.get(args, :contract_id))
-    acc = maybe_put(acc, "function", Map.get(args, :function))
-    acc = maybe_put(acc, "function_prefix", Map.get(args, :function_prefix))
-    acc
-  end
-
-  defp maybe_put(map, _k, nil), do: map
-  defp maybe_put(map, k, v), do: Map.put(map, k, v)
-
-  defp encode_bool(nil), do: nil
-  defp encode_bool(true), do: "true"
-  defp encode_bool(false), do: "false"
-
-  defp cursor_val(nil), do: nil
-  defp cursor_val({val, _rev}), do: val
-
-  defp clamp_limit(l) when is_integer(l) and l > @max_limit, do: @max_limit
-  defp clamp_limit(l) when is_integer(l) and l > 0, do: l
-  defp clamp_limit(_), do: 20
 end
--- lib/ae_mdw_web/graphql/schema/queries/contract_queries.ex
+++ lib/ae_mdw_web/graphql/schema/queries/contract_queries.ex
@@ -1,13 +1,14 @@
 defmodule AeMdwWeb.GraphQL.Schema.Queries.ContractQueries do
   use Absinthe.Schema.Notation
 
+  alias AeMdwWeb.GraphQL.Schema.Helpers.Macros
+
+  require Macros
+
   object :contract_queries do
     @desc "Contracts"
     field :contracts, :contract_page do
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 10)
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
+      Macros.pagination_args_with_scope()
       resolve(&AeMdwWeb.GraphQL.Resolvers.ContractResolver.contracts/3)
     end
 
@@ -19,57 +20,38 @@ defmodule AeMdwWeb.GraphQL.Schema.Queries.ContractQueries do
 
     @desc "Contract logs"
     field :contracts_logs, :contract_log_page do
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 10)
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
       arg(:contract_id, :string)
-      arg(:data_prefix, :string)
       arg(:event, :string)
       arg(:function, :string)
       arg(:function_prefix, :string)
+      arg(:data, :string)
       arg(:aexn_args, :boolean)
-      arg(:custom_args, :boolean)
+      Macros.pagination_args_with_scope()
       resolve(&AeMdwWeb.GraphQL.Resolvers.ContractResolver.logs/3)
+    end
+
+    @desc "Contract calls"
+    field :contracts_calls, :contract_call_page do
+      Macros.pagination_args_with_scope()
+      resolve(&AeMdwWeb.GraphQL.Resolvers.ContractResolver.calls/3)
     end
 
     @desc "Contract logs for a specific contract"
     field :contract_logs, :contract_log_page do
       arg(:id, non_null(:string))
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 10)
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
-      arg(:data_prefix, :string)
       arg(:event, :string)
       arg(:function, :string)
       arg(:function_prefix, :string)
+      arg(:data, :string)
       arg(:aexn_args, :boolean)
-      arg(:custom_args, :boolean)
+      Macros.pagination_args_with_scope()
       resolve(&AeMdwWeb.GraphQL.Resolvers.ContractResolver.contract_logs/3)
-    end
-
-    @desc "Contract calls"
-    field :contracts_calls, :contract_call_page do
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 10)
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
-      arg(:contract_id, :string)
-      arg(:function, :string)
-      arg(:function_prefix, :string)
-      resolve(&AeMdwWeb.GraphQL.Resolvers.ContractResolver.calls/3)
     end
 
     @desc "Contract calls for a specific contract"
     field :contract_calls, :contract_call_page do
       arg(:id, non_null(:string))
-      arg(:cursor, :string)
-      arg(:limit, :integer, default_value: 10)
-      arg(:from_height, :integer)
-      arg(:to_height, :integer)
-      arg(:function, :string)
-      arg(:function_prefix, :string)
+      Macros.pagination_args_with_scope()
       resolve(&AeMdwWeb.GraphQL.Resolvers.ContractResolver.contract_calls/3)
     end
   end
