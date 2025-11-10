b57f48127198e1de4e4bc3caf2d82898918c8e30
Gaith Hallak <gaithhallak@gmail.com>
2025-11-10 11:49:37 +0300

    Add query to get transactions with filter

--- lib/ae_mdw_web/graphql/resolvers/transaction_resolver.ex
+++ lib/ae_mdw_web/graphql/resolvers/transaction_resolver.ex
@@ -14,6 +14,42 @@ defmodule AeMdwWeb.GraphQL.Resolvers.TransactionResolver do
     end
   end
 
+  def transactions(_p, args, %{context: %{state: state}}) do
+    %{pagination: pagination, cursor: cursor, scope: scope} =
+      Helpers.pagination_args_with_scope(args)
+
+    query = %{}
+
+    types =
+      case Map.get(args, :type, []) ++ Map.get(args, :type_group, []) do
+        [] ->
+          nil
+
+        ls ->
+          ls
+          |> Enum.flat_map(fn type ->
+            case Validate.tx_type(to_string(type)) do
+              {:ok, valid} -> [valid]
+              {:error, _} -> []
+            end
+          end)
+          |> MapSet.new()
+      end
+
+    query = Helpers.maybe_put(query, :types, types)
+    query = Helpers.maybe_put(query, "account", Map.get(args, :account))
+    query = Helpers.maybe_put(query, "contract", Map.get(args, :contract))
+    query = Helpers.maybe_put(query, "channel", Map.get(args, :channel))
+    query = Helpers.maybe_put(query, "oracle", Map.get(args, :oracle))
+    query = Helpers.maybe_put(query, "sender_id", Map.get(args, :sender_id))
+    query = Helpers.maybe_put(query, "recipient_id", Map.get(args, :recipient_id))
+    query = Helpers.maybe_put(query, "entrypoint", Map.get(args, :entrypoint))
+
+    opts = [render_v3?: true, add_spendtx_details?: Map.has_key?(args, :account)]
+
+    Txs.fetch_txs(state, pagination, scope, query, cursor, opts) |> Helpers.make_page()
+  end
+
   def pending_transactions(_p, args, _res) do
     %{pagination: pagination, cursor: cursor} = Helpers.pagination_args(args)
 
