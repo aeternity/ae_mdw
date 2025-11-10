f3cb846c942dbe0f157e558d4043c92b73240e7d
Gaith Hallak <gaithhallak@gmail.com>
2025-11-10 13:55:30 +0300

    fix: status query

--- lib/ae_mdw_web/graphql/schema/types/status_types.ex
+++ lib/ae_mdw_web/graphql/schema/types/status_types.ex
@@ -2,16 +2,19 @@ defmodule AeMdwWeb.GraphQL.Schema.Types.StatusTypes do
   use Absinthe.Schema.Notation
 
   object :status do
-    field(:last_synced_height, non_null(:integer))
-    field(:last_key_block_hash, :string)
-    field(:last_key_block_time, :integer)
-    field(:total_transactions, :integer)
-    field(:pending_transactions, :integer)
-    field(:partial, non_null(:boolean))
-  end
-
-  object :sync_status do
-    field(:last_synced_height, non_null(:integer))
-    field(:partial, non_null(:boolean))
+    field(:node_height, :integer)
+    field(:node_version, :string)
+    field(:mdw_async_tasks, :json)
+    field(:mdw_gens_per_minute, :float)
+    field(:mdw_height, :integer)
+    field(:mdw_last_migration, :integer)
+    field(:mdw_revision, :string)
+    field(:mdw_synced, :boolean)
+    field(:mdw_syncing, :boolean)
+    field(:mdw_tx_index, :integer)
+    field(:mdw_version, :string)
+    field(:node_progress, :float)
+    field(:node_revision, :string)
+    field(:node_syncing, :boolean)
   end
 end
