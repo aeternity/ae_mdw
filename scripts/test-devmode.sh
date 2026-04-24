a3ac7b4f12ade51e731a5f774296b3ec347261a4
Ivaylo Badinov <632282+venimus@users.noreply.github.com>
2026-04-24 09:20:48 +0300

    fix: reorder dependency installation in test-devmode script

--- scripts/test-devmode.sh
+++ scripts/test-devmode.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-mix local.hex --force && mix local.rebar --force && mix deps.get
-
 export INTEGRATION_TEST=1
 export MIX_ENV=test
+
+mix local.hex --force && mix local.rebar --force && mix deps.get
 
 elixir --sname aeternity@localhost -S mix test.devmode
 
